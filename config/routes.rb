591ff16610e0c557572c42eb6f4238fb8416fd2a
cshive5 <cshive5@yahoo.com>
2015-05-22 15:23:27 -0400

    set up public folder structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  scope "/ctrp" do
+
+    get '/ctrp/', :to => redirect('/index.html')
+
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
