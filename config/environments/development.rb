6e99c341701628790677fc8defd0aff2f6c1e1b0
cshive <cshive5@yahoo.com>
2015-05-28 18:57:29 -0400

    added assets prefix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  #added by cshive
+  config.assets.prefix = "/ctrp/assets/"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
