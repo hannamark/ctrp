b5d840a246444265f98e166a11d0592cc1338452
wus4 <shenpei.wu@gmail.com>
2015-06-09 14:45:59 -0400

    CSRF setting and verification for AngularJS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  # CSRF protection for AngularJS
+  after_filter :set_csrf_cookie_for_ng
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
+  protected
+
+  def verified_request?
+    if respond_to?(:valid_authenticity_token?, true)
+      # Rails 4.2 and above
+      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+    else
+      # Rails 4.1 and below
+      super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+    end
+  end
 end
