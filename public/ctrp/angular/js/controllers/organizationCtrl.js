39a1566b765246aebe782ef33642e1106cd85b4c
rcholic <ivytony@gmail.com>
2015-06-18 14:28:31 -0400

    ui-grid added

--- public/ctrp/angular/js/controllers/organizationCtrl.js
+++ public/ctrp/angular/js/controllers/organizationCtrl.js
@@ -17,6 +17,7 @@
         vm.searchParams = OrgService.getInitialOrgSearchParams();
         activate();
 
+        //datatables plugin
         vm.dtOptions = DTOptionsBuilder.newOptions()
             .withPaginationType('full_numbers')
             .withDisplayLength(10)
@@ -35,17 +36,21 @@
             total: null
         }; //paginationOptions
 
+        //ui-grid plugin
         vm.gridOptions = {
             enableColumnResizing: true,
+            rowHeight: 50,
             paginationPageSizes: [10, 25, 50, 100],
             paginationPageSize: 10,
             useExternalPagination: true,
-            useExternalSorting: false,
+            useExternalSorting: false,  //disabled for now
             columnDefs: [
-                {field: 'name', enableSorting: true},
-                {field: 'identifier', enableSorting: true},
-                {field: 'id', enableSorting: true, displayName: 'ID'},
-                {field: 'state', enableSorting: true}
+                {field: 'name', enableSorting: true, width: '40%',
+                    cellTemplate: '<div class="tooltip-uigrid" title="{{row.getProperty(col.field)}}"><a ui-sref="main.orgDetail({orgId : \'{{row.entity.id}}\' })">View Details{{COL_FIELD CUSTOM_FILTERS}}</a></div>'
+                },
+                {field: 'identifier', enableSorting: true, width: '25%'},
+                {field: 'id', enableSorting: true, displayName: 'ID', width: '10%'},
+                {field: 'state', enableSorting: true, width: '25%'}
             ],
             onRegisterApi: function(gridApi) {
                 vm.gridApi = gridApi;
