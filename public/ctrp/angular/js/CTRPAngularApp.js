b11966caee5f262d93703450364eb141aa293204
rcholic <ivytony@gmail.com>
2015-06-19 10:16:39 -0400

    hot fix for browser cache problem and page style

--- public/ctrp/angular/js/CTRPAngularApp.js
+++ public/ctrp/angular/js/CTRPAngularApp.js
@@ -15,6 +15,22 @@
         'ui.bootstrap',
         'datatables'
     ])
+        .config(['$httpProvider', function($httpProvider) {
+            //initialize get if not there
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};
+                $httpProvider.defaults.headers.common = {};
+            }
+
+            // Answer edited to include suggestions from comments
+            // because previous version of code introduced browser-related errors
+
+            //disable IE ajax request caching
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = '0';
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }])
+
         .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
             $httpProvider.defaults.useXDomain = true;
