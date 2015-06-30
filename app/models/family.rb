09c76a7e60629b4a4e6fdaadcacabd577d5074f4
wus4 <shenpei.wu@gmail.com>
2015-06-30 11:34:04 -0400

    Delete validation for family

--- app/models/family.rb
+++ app/models/family.rb
@@ -26,4 +26,16 @@ class Family < ActiveRecord::Base
   belongs_to :family_type
 
   validates :name, uniqueness: true
+
+  before_destroy :check_for_organization
+
+  private
+
+  def check_for_organization
+    p family_memberships.size
+    unless family_memberships.size == 0
+      self.errors[:organization] << "Cannot delete Family while an Organization belongs to it."
+      return false
+    end
+  end
 end
