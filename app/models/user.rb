acfdf5aabb200d20b6fe2ec29914c74dd7b00145
saradachintala <schintal@mail.nih.gov>
2015-07-07 10:42:46 -0400

    Remove confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,7 @@ class User < ActiveRecord::Base
   #devise  :registerable,
   #       :recoverable, :trackable, :validatable,
    #      :confirmable, :lockable, :timeoutable, :omniauthable
-  devise  :confirmable, :timeoutable,  :validatable
+  devise   :timeoutable,  :validatable
 
   ROLES = %i[ROLE_ADMIN ROLE_CURATOR]
   validates :username, presence: true, uniqueness: { case_sensitive: false }
