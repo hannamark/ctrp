91f56d99d9d30d78bad2b52fe78d0df649a6e1fb
wus4 <shenpei.wu@gmail.com>
2015-07-21 17:13:41 -0400

    Log file download

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -4,4 +4,12 @@ class BackofficeController < ApplicationController
   def index
 
   end
+
+  def download_log
+    send_file(
+        "#{Rails.root}/../../logs/#{Rails.env}.log",
+        filename: "#{Rails.env}.log",
+        type: "text/plain"
+    )
+  end
 end
