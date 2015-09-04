76125a5c9fad6e9a49509a9958b9fe9fe5eb8bb3
rcholic <ivytony@gmail.com>
2015-09-04 10:06:38 -0400

    rejection code  message

--- public/ctrp/angular/js/interceptors/AuthInterceptor.js
+++ public/ctrp/angular/js/interceptors/AuthInterceptor.js
@@ -45,7 +45,7 @@
             console.log("bad response status: " + rejection.status);
             if (rejection.status > 226) {
                 $injector.get('toastr').clear();
-                $injector.get('toastr').error('Access to the resources is not authorized', 'Error');
+                $injector.get('toastr').error('Access to the resources is not authorized', 'Error Code: ' + rejection.status);
                 // $injector.get('UserService').logout();
                 console.log("error status code: " + rejection.status);
                 //redirect to login page
