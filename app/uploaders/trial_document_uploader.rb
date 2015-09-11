8d6f7fc987e13033fcf8967de4e17233d35e9127
wus4 <shenpei.wu@gmail.com>
2015-09-11 13:28:44 -0400

    Specify cashe directory

--- app/uploaders/trial_document_uploader.rb
+++ app/uploaders/trial_document_uploader.rb
@@ -17,6 +17,10 @@ class TrialDocumentUploader < CarrierWave::Uploader::Base
     "../../../storage/trial/#{model.trial_id}"
   end
 
+  def cache_dir
+    "../../../storage/tmp"
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
