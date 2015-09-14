0d74743b2c58a6f180f2179417b0df375166a4be
shivece <shivece@mail.nih.gov>
2015-09-14 17:25:28 -0400

    added milestone appsetting and refactored app version

--- app/controllers/dmz_utils_controller.rb
+++ app/controllers/dmz_utils_controller.rb
@@ -1,12 +1,15 @@
 class DmzUtilsController < ActionController::Base
 
-  def get_app_ver
-      @app_ver = Rails.configuration.application_version
+  def get_app_version
+      @app_version = Rails.configuration.application_version
+  end
+
+  def get_app_rel_milestone
+    @app_rel_milestone = AppSetting.find_by_code('APP_RELEASE_MILESTONE').value()
   end
 
   def get_login_bulletin
      @login_bulletin = AppSetting.find_by_code('LOGIN_BULLETIN').big_value()
-
   end
 
 end
