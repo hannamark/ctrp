77f4333d63648a4e86f3139e5c78a355499d292d
shivece <shivece@mail.nih.gov>
2015-09-17 14:50:54 -0400

    added better error handling around constant declarations

--- config/initializers/git_revision.rb
+++ config/initializers/git_revision.rb
@@ -9,20 +9,22 @@ module CTRP
 
     x = File.read("/local/content/ctrp/apps/ctrp/git_rev.txt")
 
-    #had to strip \n from git output
-    GIT_REVISION = x.delete("\n")
+    #hStrip \n from git output
+    GIT_REVISION ||= x.delete("\n")
 
   rescue
-      #File not present; likely executed in local dev. Get git revision by running command locally in ensure block.
-      #puts "git_rev.txt not found #{$!}"
+    #File not present; likely executed in local dev. Get git revision by running command locally in ensure block.
+    #puts "git_rev.txt not found #{$!}"
 
   ensure
     #Get git revision by running command locally from workspace
     x = `git rev-parse --short HEAD`
-    #had to strip \n from git output
-    GIT_REVISION = x.delete("\n")
-  end
 
+    #strip \n from git output
+    GIT_REVISION ||= x.delete("\n")
+
+    #GIT_REVISION ||= '9999999'
+  end
 
 
 end
