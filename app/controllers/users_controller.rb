348420730e9b47c61e3b7c7d8d0fc034cd6bf82d
saradachintala <schintal@mail.nih.gov>
2015-09-17 06:17:45 -0400

    Add permits for more fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :email, :zipcode)
+      params.require(:user).permit(:username, :email, :zipcode, :first_name, :last_name, :middle_name,  :updated_at, :role, :street_address)
     end
 end
