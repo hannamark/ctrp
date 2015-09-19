33a4582d70bdc513ae6ebee45b9517b847deffd8
saradachintala <schintal@mail.nih.gov>
2015-09-19 17:25:28 -0400

    Add signup wizard

---
+++ app/controllers/after_signup_controller.rb
@@ -0,0 +1,29 @@
+class AfterSignupController < ApplicationController
+  include Wicked::Wizard
+
+  steps :add_user_details, :add_questions
+
+
+  def show
+    Rails.logger.debug "In AfterSignUpController, show, current_local_user = #{current_local_user.inspect}"
+    @user = current_local_user
+    render_wizard
+  end
+
+  def update
+    Rails.logger.debug "In AfterSignUpController, update, current_local_user = #{current_local_user.inspect}"
+    @user = current_local_user
+ #   case step
+ #     when :confirm_password
+        @user.update_attributes(local_user_params)
+ #   end
+    #sign_in(@user, bypass: true) # needed for devise
+    render_wizard @user
+  end
+
+private
+  def local_user_params
+    params.require(:user).permit(:first_name, :last_name, :middle_name, :street_address, :zipcode)
+  end
+
+end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
-  protected
 
-  def after_sign_up_path_for(resource)
-    Rails.logger.info "In after_sign_up_path_for"
-    redirect_to users_path
+
+  def create
+    Rails.logger.info "HIIiIIIIIIII!!!!!!!"
+    super
   end
+
+  protected
+  def after_sign_up_path_for(resource)
+    Rails.logger.info "HIIiIIIIIIII!!!!!!!"
+    after_signup_path(:add_user_details)
+  end
+
+  #def after_sign_up_path_for(resource)
+    #Rails.logger.info "In after_sign_up_path_for"
+   # redirect_to users_path
+  #end
 end
