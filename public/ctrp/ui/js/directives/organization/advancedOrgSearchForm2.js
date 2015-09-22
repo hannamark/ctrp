cbe531b971ac6b0c81a7286c8a2d23fa273c5e84
rcholic <ivytony@gmail.com>
2015-09-22 16:01:21 -0400

    removed duplicate name in typeahead results

--- public/ctrp/ui/js/directives/organization/advancedOrgSearchForm2.js
+++ public/ctrp/ui/js/directives/organization/advancedOrgSearchForm2.js
@@ -73,7 +73,8 @@
 
             $scope.typeAheadNameSearch = function() {
                 var wildcardOrgName = $scope.searchParams.name.indexOf('*') > -1 ? $scope.searchParams.name : '*' + $scope.searchParams.name + '*';
-                return OrgService.searchOrgs({name: wildcardOrgName}).then(function(res) {
+                //search context: 'CTRP', to avoid duplicate names
+                return OrgService.searchOrgs({name: wildcardOrgName, source_context: "CTRP"}).then(function(res) {
                    return res.orgs.map(function(org) {
                        return org.name;
                    });
@@ -202,6 +203,7 @@
                 OrgService.getSourceContexts().then(function (contexts) {
                     //console.log("received contexts: " + JSON.stringify(contexts));
                     contexts.sort(Common.a2zComparator());
+                    console.log('contexts are: ' + JSON.stringify(contexts));
                     $scope.sourceContexts = contexts;
                 });
 
