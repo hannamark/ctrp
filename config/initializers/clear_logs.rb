b53a79ba9c3787902f631ae6ea9f2cb6e8659460
shivece <shivece@mail.nih.gov>
2015-09-25 16:18:45 -0400

    added initializer to clear out development and test logs larger tha 10MB

---
+++ config/initializers/clear_logs.rb
@@ -0,0 +1,17 @@
+#Clean out logs in development mode.
+
+if Rails.env.development?
+  MAX_LOG_SIZE = 10.megabytes
+
+  #logs = [ File.join(Rails.root, 'log', 'development.log'), File.join(Rails.root, 'log', 'test.log') ]
+
+  logs = [ File.join("/local/content/ctrp/logs/development.log"), File.join("/local/content/ctrp/logs/test.log") ]
+
+  logs.each do |log|
+    log_size = File.size?(log).to_i
+    if log_size > MAX_LOG_SIZE
+      $stdout.puts "Removing logs: #{log}"
+      `rm #{log}`
+    end
+  end
+end
