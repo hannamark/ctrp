f73c3b603e30bd3d963168f5e7a9283098a9427c
dull7295 <murali@ou.edu>
2015-09-25 15:24:49 -0400

    Chnaged MIGRATIONS

--- db/migrate/20150917192924_remove_source_cluster_id_from_organizations.rb
+++ db/migrate/20150917192924_remove_source_cluster_id_from_organizations.rb
@@ -1,5 +1,11 @@
 class RemoveSourceClusterIdFromOrganizations < ActiveRecord::Migration
-  def change
-    remove_column :organizations, :source_cluster_id, :integer
+  def down
+    add_column :organizations, :source_cluster_id, :integer
+    add_foreign_key "organizations", "source_clusters", column: "source_cluster_id"
   end
+ def up
+   remove_foreign_key :organizations, column: :source_cluster_id
+   remove_column :organizations, :source_cluster_id, :integer
+ end
+
 end
