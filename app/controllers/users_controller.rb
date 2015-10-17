757c4c3c9d40a3302bd049937b4b0b89c3c47322
saradachintala <schintal@mail.nih.gov>
2015-10-17 12:36:50 -0400

    Add email search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, :only => :index
  # before_filter :authenticate_user!, :only => :update
 
+  attr_accessor :gsa_text
+
   # GET /users
   # GET /users.json
   def index
@@ -55,6 +57,40 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def gsa
+    yml_content = YAML.load_file('config/locales/en.yml')
+
+    if local_user_signed_in?
+      Rails.logger.info "GSA for localUser"
+      @user.gsa_text = "abc"
+    elsif ldap_user_signed_in?
+      Rails.logger.info "GSA for ldapUser"
+      @user.gsa_text = "xyz"
+    else
+      Rails.logger.info "GSA for Unknown user"
+    end
+
+    respond_to do |format|
+      format.html { redirect_to @user, notice: 'User was successfully updated.' }
+    end
+=begin
+    respond_to do |format|
+      #@person.po_affiliations.destroy
+      if @user.update_attributes(user_params)
+        Rails.logger.info "@user = #{@user.inspect}"
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render json: @user}
+      else
+        #format.html { render :edit }
+        Rails.logger.info "@user= #{@user.inspect}"
+        Rails.logger.info "@user.errors = #{@user.errors.inspect}"
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+=end
+end
+
+
   def search
     Rails.logger.info "In User controller params = #{params.inspect}"
     # Pagination/sorting params initialization
@@ -64,10 +100,11 @@ class UsersController < ApplicationController
     params[:order] = 'asc' if params[:order].blank?
     @users = User.all
 
-    if params[:username].present? || params[:first_name].present? || params[:last_name].present?
+    if params[:username].present? || params[:first_name].present? || params[:last_name].present? || params[:email].present?
       @users = @users.select{|x| x.username && x.username.include?(params[:username])} if params[:username].present?
       @users = @users.select{|x| x.first_name && x.first_name.include?(params[:first_name])} if params[:first_name].present?
       @users = @users.select{|x| x.last_name && x.last_name.include?(params[:last_name])} if params[:last_name].present?
+      @users = @users.select{|x| x.email && x.email.include?(params[:email])} if params[:email].present?
     end
     Rails.logger.info "In User controller, search @users = #{@users.inspect}"
     @users
