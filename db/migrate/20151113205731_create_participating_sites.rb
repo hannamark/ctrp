db1e527d116c33f6ff65abfa7949a1aeb332038e
wus4 <shenpei.wu@gmail.com>
2015-11-12 17:24:08 -0500

    Made participating site and status one-to-many

--- db/migrate/20151113205731_create_participating_sites.rb
+++ db/migrate/20151113205731_create_participating_sites.rb
@@ -2,12 +2,13 @@ class CreateParticipatingSites < ActiveRecord::Migration
   def change
     create_table :participating_sites do |t|
       t.string :protocol_id, :limit => 255
-      t.date :status_date
       t.string :program_code, :limit => 255
+      t.string :contact_name, :limit => 255
+      t.string :contact_phone, :limit => 255
+      t.string :contact_email, :limit => 255
       t.references :trial, index: true
       t.references :organization, index: true
       t.references :person, index: true
-      t.references :site_recruitment_status, index: true
 
       t.timestamps null: false
       t.ctrp_base_columns
@@ -15,6 +16,5 @@ class CreateParticipatingSites < ActiveRecord::Migration
     add_foreign_key :participating_sites, :trials
     add_foreign_key :participating_sites, :organizations
     add_foreign_key :participating_sites, :people
-    add_foreign_key :participating_sites, :site_recruitment_statuses
   end
 end
--- test/models/participating_site_test.rb
+++ test/models/participating_site_test.rb
@@ -2,25 +2,25 @@
 #
 # Table name: participating_sites
 #
-#  id                         :integer          not null, primary key
-#  protocol_id                :string(255)
-#  status_date                :date
-#  program_code               :string(255)
-#  trial_id                   :integer
-#  organization_id            :integer
-#  person_id                  :integer
-#  site_recruitment_status_id :integer
-#  created_at                 :datetime         not null
-#  updated_at                 :datetime         not null
-#  uuid                       :string(255)
-#  lock_version               :integer          default(0)
+#  id              :integer          not null, primary key
+#  protocol_id     :string(255)
+#  program_code    :string(255)
+#  contact_name    :string(255)
+#  contact_phone   :string(255)
+#  contact_email   :string(255)
+#  trial_id        :integer
+#  organization_id :integer
+#  person_id       :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  uuid            :string(255)
+#  lock_version    :integer          default(0)
 #
 # Indexes
 #
-#  index_participating_sites_on_organization_id             (organization_id)
-#  index_participating_sites_on_person_id                   (person_id)
-#  index_participating_sites_on_site_recruitment_status_id  (site_recruitment_status_id)
-#  index_participating_sites_on_trial_id                    (trial_id)
+#  index_participating_sites_on_organization_id  (organization_id)
+#  index_participating_sites_on_person_id        (person_id)
+#  index_participating_sites_on_trial_id         (trial_id)
 #
 
 require 'test_helper'
