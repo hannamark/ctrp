28fa8fbd1f2f0a4d53cd2f39016bb2eece0e8512
rcholic <ivytony@gmail.com>
2015-11-19 10:05:24 -0500

    ctrpSubmit directive working

--- public/ctrp/ui2/src/app/modules/validators/ctrp.validateFormSubmission.directive.js
+++ public/ctrp/ui2/src/app/modules/validators/ctrp.validateFormSubmission.directive.js
@@ -2,15 +2,15 @@
 (function() {
   'use strict';
     angular.module('ctrp.module.validators')
-            .directive('ctrpFormSubmission', ctrpFormSubmission);
+            .directive('ctrpSubmit', ctrpSubmit);
 
-            ctrpFormSubmission.$inject = ['$parse', '$log'];
+            ctrpSubmit.$inject = ['$parse', '$log'];
 
-            function ctrpFormSubmission($parse, $log) {
+            function ctrpSubmit($parse, $log) {
 
                 var directiveObject = {
                     restrict: 'A',
-                    require: ['ctrpFormSubmission', '?form'],
+                    require: ['ctrpSubmit', '?form'],
                     link: linkerFn,
                     controller: controllerFn
                 };
@@ -25,21 +25,21 @@
                     scope.ctrpbtn[attrs.name] = submitController;
 
                     element.bind('submit', function(event) {
-                        submitController.setAttempted = formController.$submitted;
+                        submitController.attempted = formController.$submitted;
                         $log.info('form is submitted: ' + formController.$submitted);
                         if (!scope.$$phase) scope.$apply();
-
-                        if (formController.$invalid || formController.$pristine) {
-                            $log.error('form submission is not valid!');
+                        
+                        if (formController.$invalid) { //|| formController.$pristine
+                            $log.error('form submission invalid or untouched!');
                             return false;
-                        } else {
-                            var fn = $parse(attrs.ctrpFormSubmission);
-
-                            /* execute the ctrpFormSubmission function */
-                            scope.$apply(function() {
-                                fn(scope, {$event: event});
-                            });
                         }
+
+                        var formAction = $parse(attrs.ctrpSubmit);
+                        /* execute the form action if valid */
+                        scope.$apply(function() {
+                            formAction(scope, {$event: event});
+                        });
+
 
                     });
 
@@ -48,25 +48,35 @@
                 function controllerFn($scope, $element, $attrs) {
                     this.attempted = false;
                     var formController = null;
-                    this.setAttempted = function() {
-                        this.attempted = true;
-                    };
 
                     this.setFormController = function(controller) {
                         console.log('setting form controller!');
                         formController = controller;
                     };
 
+
+                    /* fieldModelController is ngModel controller */
                     this.needsAttention = function(fieldModelController) {
                         if (!formController) return false;
-                        $log.info('needsAttention: ', fieldModelController); // fieldModelController.$viewValue;
+                        // $log.info('fieldModelController: ', fieldModelController);
+
+                        // support passing-in validators, use the cb to alert user
+                        // fieldModelController.$$runValidators(fieldModelController.$modelValue, fieldModelController.$viewValue, cb);
+                        // $log.info('needsAttention $viewValue: ', fieldModelController.$viewValue);
 
                         if (fieldModelController) {
                             return fieldModelController.$invalid && (fieldModelController.$dirty || this.attempted);
                         } else {
                             return formController && formController.$invalid && (formController.$dirty || this.attempted);
                         }
-                    };
+                    }; //needsAttention
+
+                    /*
+                    function cb() {
+                        $log.info('cb said false!');
+                        return false;
+                    }
+                    */
                 } //controllerFn
 
 
