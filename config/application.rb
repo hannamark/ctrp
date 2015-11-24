f39a8a44bc4624ff9349983bb1cd025b1627799e
shivece <shivece@mail.nih.gov>
2015-11-24 16:31:16 -0500

    added back in static serving

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Ctrp
     config.families_id_sequence_start_with = 65000000
 
     #config.serve_static_files = true
-    paths['public'] = File.join 'public', 'ctrp', 'ui2', 'src'
+    #paths['public'] = File.join 'public', 'ctrp', 'ui2', 'src'
 
     ## Rack-cors settings for Rails
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
