3758f3b1b2db0ed21a93fb3acba9402da52ac13a
SHAMIM AHMED <shamim.ahmed@nih.gov>
2015-12-10 17:24:54 -0500

    updated po f3

---
+++ features/step_definitions/PA F01 Search Clinical Trials.js
@@ -0,0 +1,26 @@
+/**
+ * Author: Shamim Ahmed
+ * Date: 12/10/2015
+ * Feature: PA F01 Search Clinical Trials
+ */
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = require('chai').expect;
+var projectFunctionsPage= require('../support/projectMethods');
+var trialMenuItemList = require('../support/trialCommonBar');
+var addTrialPage = require('../support/registerTrialPage');
+var projectFunctionRegistryPage = require('../support/projectMethodsRegistry');
+
+
+module.exports = function() {
+    var trialMenuItem = new trialMenuItemList();
+    var addTrial = new addTrialPage();
+    var projectFunctions = new projectFunctionsPage();
+    var projectFunctionsRegistry = new projectFunctionRegistryPage();
+
+
+
+
+};
--- features/support/abstractionCommonBar.js
+++ features/support/abstractionCommonBar.js
@@ -14,7 +14,12 @@ var helperFunctions = require('../support/helper');
 
 
 var trialCommonBar = function(){
-
+    /*******
+     * Page Object: Abstraction Dashboards
+     *
+     *
+     *******/
+    var homeAbstractionDashboards = element(by.css('a[href="#/main/pa_trials"]'));
     this.homeMenu = element(by.linkText('Home'));
     this.dashboardMenu = element(by.linkText('Dashboards'));
     this.dashboardAbstractionMenu = element(by.linkText('Abstraction'));
@@ -25,89 +30,42 @@ var trialCommonBar = function(){
     this.trialsMenu = element(by.linkText('Trials'));
     this.userManagementMenu = element(by.linkText('User Management'));
 
-
-    var homeSearchTrial = element(by.css('a[href="#/main/trials"]'));
-    this.homeRegisterTrial = element(by.css('a[href="#/main/new_trial/NAT"]'));
-    this.mainTrialsLink = element(by.linkText('Trials')) ;
-    this.searchTrialLink = element(by.linkText('Search Trials'));
-    this.registerTrialLink = element(by.linkText('Register Trial'));
-    this.registerNationalTrial = element(by.css('a[href="#/main/new_trial/NAT"]'));//by.linkText('National')
-    this.registerExternallyPeerReviewedTrial = element(by.css('a[href="#/main/new_trial/EPR"]'));
-    this.registerInstitutional = element(by.css('a[href="#/main/new_trial/INS"]'));
     var pageHeaderText = element(by.css('div.row > h4'));
 
     var helper = new helperFunctions();
     var search_Trial_Header_Text = 'Search Trials * for wild card';
     var register_Trial_Header_Text = 'Register Trial';
 
+    this.clickHomeAbstractionDashboards = function(){
+        homeAbstractionDashboards.isPresent().then(function(retVal){
+            console.log('Home page Abstraction Dashboards link value is: ' + retVal);
+            if (retVal === true) {
+                helper.clickLink(homeAbstractionDashboards, "Home Search Trial link");
+                expect(pageHeaderText.getText()).to.eventually.equal(search_Trial_Header_Text);
+            }
+        });
+    };
+
     this.clickHomeMenu = function(){
-        homeMenu.isPresent().then(function(retValHome){
+        this.homeMenu.isPresent().then(function(retValHome){
             console.log('Link Exists: ' + retValHome);
             if (retValHome === true) {
-                helper.clickLink(homeSearchTrial, "Home Search Trial link");
+                helper.clickLink(this.homeMenu, "Home Menu link");
+            }
+        });
+    };
+
+    this.clickDashboardMenu = function(){
+        this.dashboardMenu.isPresent().then(function(retValDash){
+            console.log('Link Exists: ' + retValDash);
+            if (retValDash === true) {
+                helper.clickLink(this.dashboardMenu, "Home Search Trial link");
                 expect(pageHeaderText.getText()).to.eventually.equal(search_Trial_Header_Text);
             }
         });
     };
 
-    this.clickHomeSearchTrial = function(){
-        homeSearchTrial.isPresent().then(function(retVal){
-            console.log('value of ret val : ' + retVal);
-            if (retVal === true) {
-                helper.clickLink(this.homeMenu, "Home link from the Menu");
-                expect(pageHeaderText.getText()).to.eventually.equal(search_Trial_Header_Text);
-            }
-        });
-    };
 
-    this.clickHomeRegisterTrial = function(){
-        helper.clickLink(this.homeRegisterTrial, "Home Register Trial link");
-        expect(pageHeaderText.getText()).to.eventually.equal(register_Trial_Header_Text);
-    };
-
-    this.clickTrials = function(){
-        helper.clickLink(this.mainTrialsLink, "Trial link");
-    };
-
-
-    this.clickListSearchTrialLink = function(){
-        helper.clickLink(this.searchTrialLink, "Search Trial link");
-        expect(pageHeaderText.getText()).to.eventually.equal(search_Trial_Header_Text);
-    };
-
-    this.clickRegisterTrialLink = function(){
-        helper.clickLink(this.registerTrialLink, "Register Trial link");
-    };
-
-    this.clickRegisterNationalTrialLink = function(){
-        browser.get('ui/#/main/new_trial/NAT');
-      //  http://ctrp-ci.nci.nih.gov/ctrp/ui/#/main/new_trial/NAT
-      //  element(by.linkText('Register Trial')).sendKeys(protractor.Key.RIGHT);
-      ////  browser.actions().mouseMove(element(by.linkText('Search Trials')).find()).perform();
-      //  browser.sleep(10000);
-      //  //browser.driver.actions().
-      //  element(by.css('a[href="#/main/new_trial/NAT"]')).click();//.then(function(elm){
-      //    //  elm[0].click();
-      // // });
-     //   helper.clickLink(this.registerNationalTrial, "Register National Trial link");
-        expect(pageHeaderText.getText()).to.eventually.equal(register_Trial_Header_Text);
-    };
-
-    this.clickRegisterExternallyPeerReviewedTrialLink = function(){
-        browser.get('ui/#/main/new_trial/EPR');
-       // helper.clickLink(this.registerExternallyPeerReviewedTrial, "Register Externally Peer Reviewed Trial link");
-        expect(pageHeaderText.getText()).to.eventually.equal(register_Trial_Header_Text);
-    };
-
-    this.clickRegisterInstitutionalTrialLink = function(){
-        browser.get('ui/#/main/new_trial/INS');
-       // helper.clickLink(this.registerInstitutional, "Register Institutional Trial link");
-        expect(pageHeaderText.getText()).to.eventually.equal(register_Trial_Header_Text);
-    };
-
-    this.verifyRegisterTrial = function(){
-        expect(pageHeaderText.getText()).to.eventually.equal(register_Trial_Header_Text);
-    };
 
 };
 
