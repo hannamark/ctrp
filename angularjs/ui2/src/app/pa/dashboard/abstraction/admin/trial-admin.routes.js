328bfc58d0a1a77aeaead7dedc30cb2f847827b6
saradachintala <schintal@mail.nih.gov>
2015-12-18 11:40:01 -0500

    Add routes for General Trial Details and NCI specific information

---
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trial-admin.routes.js
@@ -0,0 +1,35 @@
+/**
+ * Configure routes for pa-abstraction component
+ */
+
+(function() {
+    'use strict';
+    angular.module('ctrp.module.routes').config(trialAbstractionRoutes);
+
+    trialAbstractionRoutes.$inject = ['$stateProvider'];
+    function trialAbstractionRoutes($stateProvider) {
+        $stateProvider
+                .state('main.pa.trialOverview.generalTrialDetails', {
+                    url: '/trialDetails',
+                    templateUrl: 'app/pa/dashboard/abstraction/admin/trial_details.html',
+                    // controller: 'trialIdentificationCtrl as trialIdView',
+                    section: 'pa',
+                    ncyBreadcrumb: {
+                        parent: 'main.pa.trialOverview',
+                        label: 'General Details'
+                    }
+                })
+                .state('main.pa.trialOverview.nciInfo', {
+                    url: '/nciInfo',
+                    templateUrl: 'app/pa/dashboard/abstraction/admin/trial_nci.html',
+                    section: 'pa',
+                    ncyBreadcrumb: {
+                        parent: 'main.pa.trialOverview',
+                        label: 'NCI Specific Information'
+                    }
+                });
+
+    } //trialAbstractionRoutes
+
+
+})();
---
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialNciCtrl.js
@@ -0,0 +1,17 @@
+
+/**
+ * Created by wangg5, Deember 2nd, 2015
+ */
+
+(function() {
+    'use strict';
+    angular.module('ctrp.app.pa.dashboard.admin')
+    .controller('trialNciCtrl', trialNciCtrl);
+
+    trialNciCtrl.$inject = ['$scope'];
+
+    function trialNciCtrl($scope) {
+        var vm = this;
+    } //trialNciCtrl
+
+})();
