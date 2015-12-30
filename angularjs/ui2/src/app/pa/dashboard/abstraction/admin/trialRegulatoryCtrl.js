b0510b30072868d2def94ff77983b317f2f1ba73
saradachintala <schintal@mail.nih.gov>
2015-12-30 14:56:43 -0500

    Add Investigator fields

--- angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialRegulatoryCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialRegulatoryCtrl.js
@@ -15,6 +15,9 @@
         vm.curTrial = trialDetailObj;
         vm.responsiblePartyArr = responsiblePartyObj;
         vm.countryArr = countryList;
+        vm.showInvestigator = false;
+        vm.showInvSearchBtn = true;
+
 
         vm.updateTrial = function() {
             // Prevent multiple submissions
@@ -30,6 +33,60 @@
                 vm.curTrial.investigator_aff_id = vm.selectedIaArray[0].id;
             } else {
                 vm.curTrial.investigator_aff_id = null;
+            }
+        }
+
+        vm.watchOption = function(type) {
+
+            if (type == 'responsible_party') {
+                var piOption = vm.responsiblePartyArr.filter(findPiOption);
+                var siOption = vm.responsiblePartyArr.filter(findSiOption);
+                if (piOption[0].id == vm.curTrial.responsible_party_id) {
+                    vm.showInvestigator = true;
+                    vm.showInvSearchBtn = false;
+                    vm.curTrial.investigator_title = 'Principal Investigator';
+                    // Copy the value from PI and Sponsor
+                    vm.selectedInvArray = vm.selectedPiArray;
+                    vm.selectedIaArray = vm.selectedLoArray;
+                } else if (siOption[0].id == vm.curTrial.responsible_party_id) {
+                    vm.showInvestigator = true;
+                    vm.showInvSearchBtn = true;
+                    vm.curTrial.investigator_title = 'Principal Investigator';
+                    // Copy the value from PI and Sponsor
+                    vm.selectedInvArray = vm.selectedPiArray;
+                    vm.selectedIaArray = vm.selectedSponsorArray;
+                } else {
+                    vm.showInvestigator = false;
+                    vm.curTrial.investigator_title = '';
+                    vm.selectedInvArray = [];
+                    vm.selectedIaArray = [];
+                }
+            }
+
+        };
+
+        activate();
+
+        /****************** implementations below ***************/
+        function activate() {
+            
+        }
+
+        // Return true if the option is "Principal Investigator"
+        function findPiOption(option) {
+            if (option.code == 'PI') {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Return true if the option is "Sponsor Investigator"
+        function findSiOption(option) {
+            if (option.code == 'SI') {
+                return true;
+            } else {
+                return false;
             }
         }
 
