ce50922cc737ecbf963f1952bd1270d956bd460f
rcholic <ivytony@gmail.com>
2015-12-31 10:12:52 -0500

    restructured

--- angularjs/ui2/src/app/modules/ctrp.module.constants.host.js
+++ angularjs/ui2/src/app/modules/ctrp.module.constants.host.js
@@ -3,5 +3,5 @@
     'use strict';
 
     angular.module('ctrp.module.constants')
-            .constant('HOST', 'http://localhost:3000'); //replace it with actual host server url
+            .constant('HOST', 'http://192.168.1.236:3000'); //replace it with actual host server url
 })();
