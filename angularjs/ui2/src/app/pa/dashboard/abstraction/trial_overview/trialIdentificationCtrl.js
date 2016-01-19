aef8af42d988506376a176de49f9eb6f1fa4bc1b
rcholic <ivytony@gmail.com>
2016-01-19 13:54:29 -0500

    fixes to trial idetification page for save and display

--- angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/trialIdentificationCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/trialIdentificationCtrl.js
@@ -8,9 +8,9 @@
     angular.module('ctrp.app.pa.dashboard')
     .controller('trialIdentificationCtrl', trialIdentificationCtrl);
 
-    trialIdentificationCtrl.$inject = ['$scope', 'TrialService', 'MESSAGES'];
+    trialIdentificationCtrl.$inject = ['$scope', 'TrialService', 'MESSAGES', '$timeout', '_'];
 
-    function trialIdentificationCtrl($scope, TrialService, MESSAGES) {
+    function trialIdentificationCtrl($scope, TrialService, MESSAGES, $timeout, _) {
         var vm = this;
         vm.trialProcessingObj = {comment: '', priority: ''};
         vm.saveProcessingInfo = saveProcessingInfo;
@@ -25,20 +25,25 @@
 
         function activate() {
             getTrialDetailObj();
+            _getProcessingInfo();
         }
 
         /**
          * Get trial detail object from parent scope
          */
         function getTrialDetailObj() {
-            $scope.$on(MESSAGES.TRIAL_DETAIL_SAVED, function() {
-                //get the processing info from parent scope
+            $scope.$on(MESSAGES.TRIAL_DETAIL_SAVED, _getProcessingInfo);
+        }
+
+
+        function _getProcessingInfo() {
+            $timeout(function() {
                 vm.trialProcessingObj = {
                     trialId: $scope.$parent.paTrialOverview.trialDetailObj.id,
-                    priority: $scope.$parent.paTrialOverview.trialDetailObj.process_priority || '2 - Normal',
+                    priority: _.findWhere(vm.priorities, {name: $scope.$parent.paTrialOverview.trialDetailObj.process_priority}) || 'Normal',
                     comment: $scope.$parent.paTrialOverview.trialDetailObj.process_comment
                 };
-            });
+            }, 0);
         }
 
         /* implementations below */
@@ -49,11 +54,10 @@
             updatedTrial.process_priority = vm.trialProcessingObj.priority.name;
             updatedTrial.process_comment = vm.trialProcessingObj.comment;
             console.log('updated trial: ', updatedTrial);
-            /*
+
             TrialService.upsertTrial(updatedTrial).then(function(res) {
                 console.log('priority and commented updated: ', res);
             });
-            */
         }
 
         function resetView() {
