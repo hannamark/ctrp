4a938e71e169ae090d713a0cc30386ee3195e5d9
saradachintala <schintal@mail.nih.gov>
2016-01-27 21:46:38 -0500

    Add collaborator specific code

--- angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialCollaboratorsCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialCollaboratorsCtrl.js
@@ -8,11 +8,121 @@
     angular.module('ctrp.app.pa.dashboard')
     .controller('trialCollaboratorsCtrl', trialCollaboratorsCtrl);
 
-    trialCollaboratorsCtrl.$inject = ['TrialService', 'PATrialService', '$scope', '$timeout','$state', 'toastr', 'MESSAGES', 'trialDetailObj', 'studySourceObj', 'nciDivObj', 'nciProgObj'];
+    trialCollaboratorsCtrl.$inject = ['TrialService', 'PATrialService', '$scope', '$timeout','$state', 'toastr', 'MESSAGES', 'trialDetailObj'];
 
-    function trialCollaboratorsCtrl(TrialService, PATrialService, $scope, $timeout, $state, toastr, MESSAGES,trialDetailObj, studySourceObj, nciDivObj, nciProgObj) {
+    function trialCollaboratorsCtrl(TrialService, PATrialService, $scope, $timeout, $state, toastr, MESSAGES, trialDetailObj) {
+
+        var vm = this;
+        vm.curTrial = trialDetailObj;
+        console.log("trialDetailObj = " + JSON.stringify(trialDetailObj));
+        console.log("pa_editable = " + JSON.stringify(trialDetailObj["pa_editable"]));
+        vm.addedCollaborators = [];
+        vm.selectedCollaborators = [];
+        vm.collaboratorsNum = 0;
+
+        vm.updateTrial = function(updateType) {
+            // Prevent multiple submissions
+            vm.disableBtn = true;
+
+            // An outer param wrapper is needed for nested attributes to work
+            var outerTrial = {};
+            outerTrial.new = vm.curTrial.new;
+            outerTrial.id = vm.curTrial.id;
+            outerTrial.trial = vm.curTrial;
+
+            if (vm.addedCollaborators.length > 0) {
+                vm.curTrial.collaborator_attributes = [];
+                _.each(vm.addedCollaborators, function (collaborator) {
+                    vm.curTrial.collaborator_attributes.push(collaborator);
+                });
+            }
+
+            console.log("outertrial IN SAVE! " + JSON.stringify(outerTrial));
 
 
+            TrialService.upsertTrial(outerTrial).then(function(response) {
+                //toastr.success('Trial ' + vm.curTrial.lead_protocol_id + ' has been recorded', 'Operation Successful!');
+                vm.curTrial.lock_version = response.lock_version || '';
+                //PATrialService.setCurrentTrial(vm.curTrial); // update to cache
+                $scope.$emit('updatedInChildScope', {});
+
+                toastr.clear();
+                toastr.success('Trial ' + vm.curTrial.lead_protocol_id + ' has been recorded', 'Operation Successful!', {
+                    extendedTimeOut: 1000,
+                    timeOut: 0
+                });
+            }).catch(function(err) {
+                console.log("error in updating trial " + JSON.stringify(outerTrial));
+            });
+
+        } // updateTrial
+
+        vm.reload = function() {
+            $state.go($state.$current, null, { reload: true });
+        };
+
+        // Delete the associations
+        vm.toggleSelection = function (index) {
+            if (index < vm.addedCollaborators.length) {
+                vm.addedCollaborators[index]._destroy = !vm.addedCollaborators[index]._destroy;
+                if (vm.addedCollaborators[index]._destroy) {
+                    vm.collaboratorsNum--;
+                } else {
+                    vm.collaboratorsNum++;
+                }
+            }
+        }
+
+
+        // Add Collaborator to a temp array
+        $scope.$watch(function() {
+            //console.log("1curTrial =" + JSON.stringify(vm.curTrial));
+            return vm.selectedCollaborators.length;
+        }, function(newValue, oldValue) {
+            if (newValue == oldValue + 1) {
+                var newCollaborator = {};
+                newCollaborator.organization_id = vm.selectedCollaborators[vm.selectedCollaborators.length - 1].id;
+                newCollaborator.organization_name = vm.selectedCollaborators[vm.selectedCollaborators.length - 1].name;
+                newCollaborator._destroy = false;
+                vm.addedCollaborators.push(newCollaborator);
+                vm.collaboratorsNum++;
+            }
+        });
+
+        /****************** implementations below ***************/
+        function activate() {
+            appendCollaborators();
+            getTrialDetailCopy();
+            watchTrialDetailObj();
+        }
+
+        /**
+         * Get trial detail object from parent scope
+         */
+        function watchTrialDetailObj() {
+            $scope.$on(MESSAGES.TRIAL_DETAIL_SAVED, function() {
+                getTrialDetailCopy();
+            });
+        } //watchTrialDetailObj
+
+        function getTrialDetailCopy() {
+            $timeout(function() {
+                vm.curTrial = PATrialService.getCurrentTrialFromCache();
+                console.log("vm.curTrial =" + JSON.stringify(vm.curTrial ));
+            }, 1);
+        } //getTrialDetailCopy
+
+        function appendCollaborators() {
+            for (var i = 0; i < vm.curTrial.collaborators.length; i++) {
+                var viewCollaborator = {};
+                viewCollaborator.id = vm.curTrial.collaborators[i].id;
+                viewCollaborator.organization_id = vm.curTrial.collaborators[i].organization_id;
+                viewCollaborator.organization_name = vm.curTrial.collaborators[i].organization_name;
+                viewCollaborator._destroy = false;
+                vm.addedCollaborators.push(viewCollaborator);
+                vm.collaboratorsNum++;
+            }
+        }
 
     } //trialCollaboratorsCtrl
 
