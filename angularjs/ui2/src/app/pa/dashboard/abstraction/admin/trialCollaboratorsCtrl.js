fbde3a83727723f817d7b7ecf0702610a03fae22
saradachintala <schintal@mail.nih.gov>
2016-02-04 13:45:42 -0500

    invoke activate

--- angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialCollaboratorsCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialCollaboratorsCtrl.js
@@ -115,6 +115,8 @@
             }
         });
 
+        activate();
+
         /****************** implementations below ***************/
         function activate() {
             appendCollaborators();
