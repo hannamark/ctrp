97fe08a8f5043b5a7b72d34fda18b898b5125844
dull7295 <murali@ou.edu>
2016-02-05 10:25:42 -0500

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -5,27 +5,31 @@ Rails.application.routes.draw do
   resources :trial_documents
 
 
-namespace "ws" do
-  scope '/api' do
-    scope '/v1' do
-      scope '/trials' do
-        scope '/complete' do
-          get '/' => 'api_trials#index'
-          get '/:id' =>  'api_trials#show'
-          post '/' => 'api_trials#create'
-          post '/:idType/:id' => 'api_trials#update'
-          post '/:id' => 'api_trials#update'
-          put '/:idType/:id' => 'api_trials#amend'
-          put  '/:id' =>  'api_trials#update'
-          put  '/:id/status' =>  'api_trials#change_status'
-        end
-      end
-    end
-  end
-end
+
 
 
   scope "/ctrp" do
+
+    namespace "ws" do
+      scope '/api' do
+        scope '/v1' do
+          scope '/trials' do
+            scope '/complete' do
+              get '/' => 'api_trials#index'
+              get '/:id' =>  'api_trials#show'
+              post '/' => 'api_trials#create'
+              post '/:idType/:id' => 'api_trials#update'
+              post '/:id' => 'api_trials#update'
+              put '/:idType/:id' => 'api_trials#amend'
+              put  '/:id' =>  'api_trials#update'
+              put  '/:id/status' =>  'api_trials#change_status'
+            end
+          end
+        end
+      end
+    end
+
+
     devise_for :users
     root 'ctrp#index'
 
