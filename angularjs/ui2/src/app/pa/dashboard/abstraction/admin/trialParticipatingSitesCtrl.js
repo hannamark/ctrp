0afd9b45e6377bf210a81220880bdffd488cd827
saradachintala <schintal@mail.nih.gov>
2016-02-08 10:57:26 -0500

    Initial code for edit participating site

--- angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialParticipatingSitesCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialParticipatingSitesCtrl.js
@@ -14,12 +14,43 @@
 
         var vm = this;
         vm.curTrial = trialDetailObj;
-        console.log("trialDetailObj = " + JSON.stringify(trialDetailObj));
-        console.log("pa_editable = " + JSON.stringify(trialDetailObj["pa_editable"]));
-        console.log("participating_sites_list = " + JSON.stringify(trialDetailObj["participating_sites_list"]));
+        vm.setAddMode = setAddMode;
+        vm.setEditMode = setEditMode;
+        vm.currentParticipatingSite= {};
+        //vm.saveCurrentParticipatingSite = saveGeneralTrialDetails;
+        //vm.resetCurrentParticipatingSite = resetGeneralTrialDetails;
+        vm.currentParticipatingSite.organization = {name: '', array: []};
 
-        vm.updateTrial = function(updateType) {
+        /*
+         * This function is invoked when the organizations are added and saved
+         */
+        vm.updateTrial = function() {
             // Prevent multiple submissions
+            console.log("update Trial ");
+            if (vm.addedParticipatingSites.length > 0) {
+                vm.curTrial.participating_sites_attributes = [];
+                _.each(vm.addedParticipatingSites, function (participating_site) {
+                    var exists = false
+                    for (var i = 0; i < vm.curTrial.participating_sites.length; i++) {
+                        if (vm.curTrial.participating_sites[i].id) {
+                            if (vm.curTrial.participating_sites[i].organization_id == participating_site.organization_id) {
+                                exists = true;
+                            }
+                        }
+                    }
+                    console.log("update Trial exists ="+exists);
+                    if (!exists){
+                        vm.curTrial.participating_sites_attributes.push(collaborator);
+                    }
+                });
+            }
+            console.log("vm.curTrial.collaborators_attributes " + JSON.stringify(vm.curTrial.collaborators_attributes));
+            vm.saveTrial();
+            vm.addedCollaborators = [];
+
+        } // updateTrial
+
+        vm.saveTrial = function(){
             vm.disableBtn = true;
 
             // An outer param wrapper is needed for nested attributes to work
@@ -28,17 +59,13 @@
             outerTrial.id = vm.curTrial.id;
             outerTrial.trial = vm.curTrial;
 
-
-            //console.log("outertrial IN SAVE! " + JSON.stringify(outerTrial));
-
-
             TrialService.upsertTrial(outerTrial).then(function(response) {
                 //toastr.success('Trial ' + vm.curTrial.lead_protocol_id + ' has been recorded', 'Operation Successful!');
                 vm.curTrial.lock_version = response.lock_version || '';
                 //toastr.success('Trial ' + vm.curTrial.lead_protocol_id + ' has been recorded', 'Operation Successful!');
                 PATrialService.setCurrentTrial(vm.curTrial); // update to cache
                 $scope.$emit('updatedInChildScope', {});
-
+                vm.curTrial.collaborators = response["collaborators"];
                 toastr.clear();
                 toastr.success('Trial ' + vm.curTrial.lead_protocol_id + ' has been recorded', 'Operation Successful!', {
                     extendedTimeOut: 1000,
@@ -48,17 +75,44 @@
                 console.log("error in updating trial " + JSON.stringify(outerTrial));
             });
 
-        } // updateTrial
+        }//saveTrial
+
 
         vm.reload = function() {
             $state.go($state.$current, null, { reload: true });
         };
 
+        // Add Participating to a temp array
+        function watchOrganization() {
+            $scope.$watchCollection(function() {return vm.currentParticipatingSite.organization.array;}, function(newVal, oldVal) {
+                if (angular.isArray(newVal) && newVal.length > 0) {
+                    vm.currentParticipatingSite.name = newVal[0].name;
+                    vm.currentParticipatingSite.organization = newVal[0];
+                    vm.currentParticipatingSite.organization_id = newVal[0].id; // update sponsor
+                }
+            });
+        }
+
+        function setAddMode() {
+            console.log("SETTING TO ADDMODE");
+            vm.addEditMode = true;
+        }
+
+        function setEditMode(idx) {
+            vm.addEditMode = true;
+            vm.currentParticipatingSite = vm.curTrial.participating_sites_list[idx];
+            console.log("SETTING TO EDITMODE vm.currentParticipatingSite="+JSON.stringify(vm.currentParticipatingSite));
+            vm.currentParticipatingSite.organization = {name: vm.currentParticipatingSite["po_name"], array: []};
+        }
+
+        activate();
+
         /****************** implementations below ***************/
         function activate() {
-            appendCollaborators();
+            //appendCollaborators();
             getTrialDetailCopy();
             watchTrialDetailObj();
+            watchOrganization();
         }
 
         /**
@@ -76,6 +130,7 @@
                 console.log("vm.curTrial =" + JSON.stringify(vm.curTrial ));
             }, 1);
         } //getTrialDetailCopy
+
 
     } //trialParticipatingSitesCtrl
 
