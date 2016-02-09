11c8784a0dbfd7d2fa5b13d8fa9bff2c67575492
SHAMIM AHMED <shamim.ahmed@nih.gov>
2016-02-09 14:29:48 -0500

    added paa f09

---
+++ features/step_definitions/PAA F09 Add and Edit Trial Collaborators.js
@@ -0,0 +1,107 @@
+/**
+ * Author: Shamim Ahmed
+ * Date: 02/09/2016
+ * Feature: PAA F09 Add and Edit Trial Collaborators
+ *
+ * Note: In the PAA search screen it has dependency on the seed data
+ */
+
+//Common dependencies
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = require('chai').expect;
+var moment = require ('moment');
+
+//Required dependencies
+//Login dependencies
+var loginPage = require('../support/LoginPage');
+//helper methods
+var helperMethods = require('../support/helper');
+//Project Related methods dependencies
+var projectFunctionMethods= require('../support/projectMethods');
+//Menu bar dependencies
+var abstractionPageMenu = require('../support/abstractionCommonBar');
+//Abstraction search page dependencies
+var abstractionTrialSearchPage = require('../support/abstractionSearchTrialPage');
+//Abstraction common dependencies
+var abstractionCommonMethods = require('../support/abstractionCommonMethods');
+//Abstraction NCI Specific Information
+var abstractionNCISpecific = require('../support/abstractionNCISpecificInfo');
+//List of Organization
+var OrgPage = require('../support/ListOfOrganizationsPage');
+//Organization Search
+var orgSearch = require('../support/abstractionOrganizationSearch');
+//Regulatory Information - IND/IDE
+var abstractionRegulatoryINDIDE = require('../support/abstractionRegulatoryIND');
+//Regulatory Information - Human Subject Safety
+var abstractionRegulatoryHuman = require('../support/abstractionRegulatoryHuman');
+//
+var projectFunctionsPage= require('../support/projectMethods');
+var addTrialPage = require('../support/registerTrialPage');
+var projectFunctionRegistryPage = require('../support/projectMethodsRegistry');
+
+
+module.exports = function() {
+
+    var login = new loginPage();
+    var helper = new helperMethods();
+    var projectFunctions = new projectFunctionMethods();
+    var commonFunctions = new abstractionCommonMethods();
+    var pageMenu = new abstractionPageMenu();
+    var pageSearchTrail = new abstractionTrialSearchPage();
+    var nciSpecific = new abstractionNCISpecific();
+    var indIDE = new abstractionRegulatoryINDIDE();
+    var humanSafety = new abstractionRegulatoryHuman();
+    var searchOrg = new OrgPage();
+    var organizationSearch = new orgSearch();
+    var addTrial = new addTrialPage();
+    //var projectFunctions = new projectFunctionsPage();
+    var projectFunctionsRegistry = new projectFunctionRegistryPage();
+    var searchTableHeader = '';
+    var nciID = 'NCI-2014-00894';
+    var randNmbr = Math.floor(Math.random()*(95-77+1)+77);
+    var leadProtocolID = 'CTRP_01_1776';
+    var leadProtocolIDA = 'CTRP_01_1777';
+    var leadProtocolIDB = 'CTRP_01_1778';
+    var leadProtocolIDC = 'CTRP_01_17'+randNmbr;
+    var leadProtocolIDD = 'CTRP_01_1781';
+    var searchResultCountText = 'Trial Search Results';
+    var indIDEAssociatedQueVal = '';
+    var indTypVal = 'IND';
+    var ideTypVal = 'IDE';
+    var aprvlNumberRnd = Math.floor(Math.random()*(99999999-77777777+1)+77777777).toString();
+    var boardApprovNmbr = '';
+    var getBoardName = 'TestBoardName'+moment().format('MMMDoYY hmmss');
+    var boardNm = '';
+    var orgSearchNameA = 'Boston Medical Center';
+    var orgSearchNameB = 'Boston University School Of Public Health';
+    var buildSelectionOpton = '';
+    var indIDENmbrA = 'BBIND13794';
+    var indIDENmbrB = 'IND108498';
+    var indIDENmbrC = '102902';
+    var boardApprovalStatusCrntVal = '';
+    var boardApprovalStatusSelect = 'Select a status';
+    var boardApprovalStatusPending = 'Submitted, pending';
+    var boardApprovalStatusApproved = 'Submitted, approved';
+    var boardApprovalStatusExempt = 'Submitted, exempt';
+    var boardApprovalStatusDenied = 'Submitted, denied';
+    var boardApprovalStatusNotRequired = 'Submission not required';
+    var optionA = '';
+    var optionB = '';
+    var optionC = '';
+    var optionD = '';
+    var optionE = '';
+    var optionF = '';
+    var optionG = '';
+    var optionH = '';
+    var optionI = '';
+    var affiOrg = 'Affiliated Organization';
+    var requiredMsgApproval = 'Board approval number is required';
+    var requiredMsgAffiliation = 'Board affiliation is required';
+    var requiredMsgName = 'Board name is required';
+
+
+
+
+};
