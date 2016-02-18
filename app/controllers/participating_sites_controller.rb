676396d25be21c9984d36d8e1ae6a68fe1563b6e
saradachintala <schintal@mail.nih.gov>
2016-02-17 22:14:13 -0500

    Fix add Participating site code

--- app/controllers/participating_sites_controller.rb
+++ app/controllers/participating_sites_controller.rb
@@ -70,7 +70,7 @@ class ParticipatingSitesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def participating_site_params
 
-      params[:participating_site].permit(:protocol_id, :program_code, :contact_name, :contact_phone, :contact_email, :trial_id, :organization_id,
+      params[:participating_site].permit(:id, :protocol_id, :program_code, :contact_name, :contact_phone, :contact_email, :trial_id, :organization_id,
     site_rec_status_wrappers_attributes: [:id,:status_date, :site_recruitment_status_id, :_destroy]
     # participating_site_investigators: [:person_id, :set_as_contact, :investigator_type]
     )
