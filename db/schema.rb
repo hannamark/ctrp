a4b0e708e9202e059b6a354c0fe05246df4180cc
dull7295 <murali@ou.edu>
2016-02-20 14:11:45 -0500

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -1063,9 +1063,9 @@ ActiveRecord::Schema.define(version: 20160218040543) do
     t.integer  "item_id",        null: false
     t.string   "event",          null: false
     t.string   "whodunnit"
-    t.text     "object"
+    t.jsonb    "object"
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.jsonb    "object_changes"
     t.integer  "transaction_id"
   end
 
@@ -1250,9 +1250,9 @@ ActiveRecord::Schema.define(version: 20160218040543) do
     t.integer  "item_id",        null: false
     t.string   "event",          null: false
     t.string   "whodunnit"
-    t.text     "object"
+    t.jsonb    "object"
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.jsonb    "object_changes"
     t.integer  "transaction_id"
   end
 
