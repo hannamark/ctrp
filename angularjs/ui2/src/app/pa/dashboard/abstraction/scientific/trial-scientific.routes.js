2ccc099175edec77039b0342580c8f3c837f54a4
saradachintala <schintal@mail.nih.gov>
2016-03-07 21:18:08 -0500

    Add initial setup for anatomic sites

--- angularjs/ui2/src/app/pa/dashboard/abstraction/scientific/trial-scientific.routes.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/scientific/trial-scientific.routes.js
@@ -27,21 +27,38 @@
                         label: 'Trial Description'
                     }
                 })
-                .state('main.pa.trialOverview.trialDesign', {
-                    url: '/trial-design',
-                    templateUrl: 'app/pa/dashboard/abstraction/scientific/pas_trial_design.html',
-                    controller: 'pasTrialDesignCtrl as designView',
-                    resolve: {
-                        PATrialService: 'PATrialService',
-                        groupedTrialDesignData: function(PATrialService) {
-                            return PATrialService.groupTrialDesignData();
-                        }
-                    },
-                    section: 'pa',
-                    ncyBreadcrumb: {
-                        parent: 'main.pa.trialOverview',
-                        label: 'Trial Design'
+            .state('main.pa.trialOverview.trialDesign', {
+                url: '/trial-design',
+                templateUrl: 'app/pa/dashboard/abstraction/scientific/pas_trial_design.html',
+                controller: 'pasTrialDesignCtrl as designView',
+                resolve: {
+                    PATrialService: 'PATrialService',
+                    groupedTrialDesignData: function(PATrialService) {
+                        return PATrialService.groupTrialDesignData();
                     }
-                });
+                },
+                section: 'pa',
+                ncyBreadcrumb: {
+                    parent: 'main.pa.trialOverview',
+                    label: 'Trial Design'
+                }
+            })
+            .state('main.pa.trialOverview.anatomicSites', {
+                url: '/anatomic-sites',
+                templateUrl: 'app/pa/dashboard/abstraction/scientific/pas_anatomic_sites.html',
+                controller: 'pasAnatomicSitesCtrl as anatomicView',
+                resolve: {
+                    TrialService: 'TrialService',
+                    PATrialService: 'PATrialService',
+                    trialDetailObj: function($stateParams, TrialService) { 
+                        return TrialService.getTrialById($stateParams.trialId);
+                    },
+                },
+                section: 'pa',
+                ncyBreadcrumb: {
+                    parent: 'main.pa.trialOverview',
+                    label: 'Anatomic Sites'
+                }
+            });
     }
 })();
