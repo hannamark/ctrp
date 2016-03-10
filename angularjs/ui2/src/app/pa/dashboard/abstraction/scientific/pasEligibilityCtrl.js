93dc6694426c2f48f28d2d45741f0bc513903f27
rcholic <ivytony@gmail.com>
2016-03-10 16:12:40 -0500

    finished Eligibility

--- angularjs/ui2/src/app/pa/dashboard/abstraction/scientific/pasEligibilityCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/scientific/pasEligibilityCtrl.js
@@ -16,7 +16,7 @@
         vm.genderList = genderList;
         vm.ageUnits = ageUnits;
         console.info(genderList, ageUnits);
-        
+
         vm.updateCriteria = updateCriteria;
         vm.resetForm = resetForm;
 
@@ -35,7 +35,31 @@
         }
 
         function updateCriteria() {
-            console.info('updateCriteria');
+            var outerTrial = {};
+            outerTrial.new = false;
+            outerTrial.id = vm.trialDetailObj.id;
+            outerTrial.trial = vm.trialDetailObj;
+            outerTrial.trial.lock_version = PATrialService.getCurrentTrialFromCache().lock_version;
+            PATrialService.upsertTrial(outerTrial).then(function(res) {
+
+                if (res.server_response.status === 200) {
+                    vm.trialDetailObj = res;
+                    vm.trialDetailObj.lock_version = res.lock_version;
+
+                    PATrialService.setCurrentTrial(vm.trialDetailObj); // update to cache
+                    $scope.$emit('updatedInChildScope', {});
+
+                    toastr.clear();
+                    toastr.success('Eligibility Criteria has been updated', 'Successful!', {
+                        extendedTimeOut: 1000,
+                        timeOut: 0
+                    });
+                    _getTrialDetailCopy();
+                }
+
+            }).catch(function(err) {
+                console.error('error in updating trial design: ', err);
+            });
         }
 
     } // pasEligibilityCtrl
