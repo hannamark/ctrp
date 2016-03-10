bcb309da2c1b6aea07860297010d6d46720550be
wus4 <shenpei.wu@gmail.com>
2016-03-10 13:24:27 -0500

    Null safe for user names

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -502,7 +502,7 @@ class Trial < TrialBase
 
         mail_template.body_html.sub!('${submissionDate}', last_submission.submission_date.strftime('%d-%b-%Y')) if last_submission.submission_date.present?
         mail_template.body_html.sub!('${CurrentDate}', Date.today.strftime('%d-%b-%Y'))
-        mail_template.body_html.sub!('${SubmitterName}', last_submission.user.first_name + ' ' + last_submission.user.last_name) if last_submission.user.present?
+        mail_template.body_html.sub!('${SubmitterName}', last_submission.user.first_name + ' ' + last_submission.user.last_name) if last_submission.user.present? && last_submission.user.first_name.present? && last_submission.user.last_name.present?
         mail_template.body_html.sub!('${nciTrialIdentifier}', self.nci_id) if self.nci_id.present?
 
         CtrpMailer.general_email(mail_template.from, mail_template.to, mail_template.cc, mail_template.bcc, mail_template.subject, mail_template.body_text, mail_template.body_html).deliver_now
