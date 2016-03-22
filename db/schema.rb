142d36afd45bdbaedcccc2424d4b1f28d6d0e25d
wus4 <shenpei.wu@gmail.com>
2016-03-22 10:12:32 -0400

    Schema differenct on version tables

--- db/schema.rb
+++ db/schema.rb
@@ -1205,9 +1205,9 @@ ActiveRecord::Schema.define(version: 20160321201358) do
     t.integer  "item_id",        null: false
     t.string   "event",          null: false
     t.string   "whodunnit"
-    t.text     "object"
+    t.jsonb    "object"
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.jsonb    "object_changes"
     t.integer  "transaction_id"
   end
 
@@ -1403,9 +1403,9 @@ ActiveRecord::Schema.define(version: 20160321201358) do
     t.integer  "item_id",        null: false
     t.string   "event",          null: false
     t.string   "whodunnit"
-    t.text     "object"
+    t.jsonb    "object"
     t.datetime "created_at"
-    t.text     "object_changes"
+    t.jsonb    "object_changes"
     t.integer  "transaction_id"
   end
 
