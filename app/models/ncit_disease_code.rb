334197c8f40a16e33ea459d3b106a68aaf06e69e
wus4 <shenpei.wu@gmail.com>
2016-03-23 11:42:50 -0400

    Import NCI Thesaurus method initial check-in

--- app/models/ncit_disease_code.rb
+++ app/models/ncit_disease_code.rb
@@ -35,15 +35,78 @@ class NcitDiseaseCode < ActiveRecord::Base
 
     url = AppSetting.find_by_code('NCI_THESAURUS_URL').value
     file_names = AppSetting.find_by_code('NCI_THESAURUS_FILES').big_value.split(',')
+    act = NcitStatus.find_by_code('ACT')
 
     file_names.each do |file_name|
       File.open("../../storage/ncit/#{file_name}", 'wb') do |fo|
         fo.write open(url + file_name).read
       end
 
+      # Populate ncit_disease_codes and ncit_disease_synonyms table
       Zip::File.open("../../storage/ncit/#{file_name}") do |zipfile|
         zipfile.each do |entry|
           xml = Nokogiri::XML(entry.get_input_stream.read)
+          # Only search for owl:Class element with rdf:about attribute
+          xml.xpath('//owl:Class[@rdf:about]').each do |node|
+            nt_term_id = node.xpath('@rdf:about').text
+            if nt_term_id[0] == '#'
+              nt_term_id.slice!(0)
+            end
+
+            if !NcitDiseaseCode.exists?(nt_term_id: nt_term_id)
+              name = node.xpath('rdfs:label').text
+              new_disease_code = NcitDiseaseCode.create(nt_term_id: nt_term_id, preferred_name: name, menu_display_name: name, ncit_status: act)
+
+              node.xpath('P90/ncicp:ComplexTerm/ncicp:term-name').each do |synonym|
+                if !NcitDiseaseSynonym.exists?(alternate_name: synonym.text, ncit_disease_code: new_disease_code)
+                  NcitDiseaseSynonym.create(alternate_name: synonym.text, ncit_status: act, ncit_disease_code: new_disease_code)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    file_names.each do |file_name|
+      # Populate ncit_disease_parents table
+      Zip::File.open("../../storage/ncit/#{file_name}") do |zipfile|
+        zipfile.each do |entry|
+          xml = Nokogiri::XML(entry.get_input_stream.read)
+          # Only search for owl:Class element with rdf:about attribute
+          xml.xpath('//owl:Class[@rdf:about]').each do |node|
+            nt_term_id = node.xpath('@rdf:about').text
+            if nt_term_id[0] == '#'
+              nt_term_id.slice!(0)
+            end
+
+            child_record = NcitDiseaseCode.find_by_nt_term_id(nt_term_id)
+            if child_record.present?
+              node.xpath('owl:equivalentClass/owl:Class/owl:intersectionOf/rdf:Description/@rdf:about').each do |about|
+                parent = about.text
+                if parent[0] == '#'
+                  parent.slice!(0)
+                end
+
+                parent_record = NcitDiseaseCode.find_by_nt_term_id(parent)
+                if parent_record.present? && !NcitDiseaseParent.exists?(child: child_record, parent: parent_record)
+                  NcitDiseaseParent.create(ncit_status: act, child: child_record, parent: parent_record)
+                end
+              end
+
+              node.xpath('rdfs:subClassOf/@rdf:resource').each do |resource|
+                parent = resource.text
+                if parent[0] == '#'
+                  parent.slice!(0)
+                end
+
+                parent_record = NcitDiseaseCode.find_by_nt_term_id(parent)
+                if parent_record.present? && !NcitDiseaseParent.exists?(child: child_record, parent: parent_record)
+                  NcitDiseaseParent.create(ncit_status: act, child: child_record, parent: parent_record)
+                end
+              end
+            end
+          end
         end
       end
     end
