987b7b75880ffe6980b00ec4efa3f2e03ce6941c
wus4 <shenpei.wu@gmail.com>
2016-03-24 16:54:54 -0400

    smtp server change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      address: "mailfwd.nih.gov",
+      address: "Mailfwd.hub.nih.gov",
       port: 25,
       authentication: "plain"
   }
