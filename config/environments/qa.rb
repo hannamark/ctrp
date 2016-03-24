2cec8929522b597ffc29081061633668fb317fdc
Radhika Tekumalla <radhika.tekumalla@nih.gov>
2016-03-24 15:23:35 -0400

    Modfied SMTP server in QA config file

--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      address: "mailfwd.nih.gov",
+      address: "mailfwd.hub.nih.gov",
       port: 25,
       authentication: "plain"
   }
