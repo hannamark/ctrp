512fd2534b48eb483b47233a2ba6eb2af1e05130
dull7295 <murali@ou.edu>
2016-04-03 22:01:19 -0400

    ...

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -469,11 +469,11 @@ class Trial < TrialBase
 
   def create_ownership
     # New Trial Ownership
-    if self.coming_from == 'rest'
-      TrialOwnership.create(trial: self, user: User.find_by_username("ctrptrialsubmitter"))
-    else
+    #if self.coming_from == 'rest'
+     # TrialOwnership.create(trial: self, user: User.find_by_username("ctrptrialsubmitter"))
+    #else
       TrialOwnership.create(trial: self, user: self.current_user) if self.current_user.present?
-    end
+    #end
   end
 
   def send_email
