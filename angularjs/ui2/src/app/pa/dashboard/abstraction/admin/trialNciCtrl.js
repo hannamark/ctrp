123621e1bdc1a8e38e9d5bcf67b7f136ef3e1535
rcholic <ivytony@gmail.com>
2016-04-12 15:00:48 -0400

    cleaned code

--- angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialNciCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/admin/trialNciCtrl.js
@@ -27,11 +27,9 @@
         vm.study_source_id = vm.curTrial.study_source_id;
 
         var sponsorName = Common.valueAtPathInObject(trialDetailObj, 'sponsor.name');
-        console.info('sponsorName: ', sponsorName);
-        vm.isSponsorNci = sponsorName == "National Cancer Institute" ? true: false;
+        vm.isSponsorNci = sponsorName === "National Cancer Institute" ? true: false;
         var leadOrgName = Common.valueAtPathInObject(trialDetailObj, 'lead_org.name');
-        console.info('leadOrgName: ', leadOrgName);
-        vm.isLeadOrgNciCcr = leadOrgName == "NCI - Center for Cancer Research" ? true: false;
+        vm.isLeadOrgNciCcr = leadOrgName === "NCI - Center for Cancer Research" ? true: false;
 
         if (((vm.isSponsorNci==true) && (vm.isLeadOrgNciCcr==true) && (trialDetailObj.send_trial_rules_flag == "Yes"))==true) {
             vm.disable_send_trial = false;
