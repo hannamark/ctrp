4fb254d6def96616cc04270e6a8fe33e7d86e25d
rcholic <ivytony@gmail.com>
2016-04-18 11:27:24 -0400

    moved back to controller

--- angularjs/ui2/src/app/modules/widgets/trial/ctrp.widgets.imported-ncit-interventions-search.directive.js
+++ angularjs/ui2/src/app/modules/widgets/trial/ctrp.widgets.imported-ncit-interventions-search.directive.js
@@ -20,8 +20,8 @@
         scope: {
           maxRowSelectable: '=?'
         },
-        // controller: nciInterventionsSearchCtrl,
-        // controllerAs: 'interventionsLookupView',
+        controller: nciInterventionsSearchCtrl,
+        controllerAs: 'interventionsLookupView',
         templateUrl: 'app/modules/widgets/trial/ctrp.imported-ncit-interventions-search-template.html'
       };
 
@@ -29,39 +29,47 @@
 
       function linkerFn(scope, element, attrs, ngModelCtrl) {
           console.info('in linkerFn: ', scope.maxRowSelectable);
-          // scope.lookupInterventions = lookupInterventions;
-          // scope.resetSearch = resetSearch;
-          scope.searchParams = _getSearchParams();
-          scope.searchResults = _initResultsObj();
-          scope.gridOptions = getGridOptions();
-          $compile(element.contents())(scope); // compile the template
+         // $compile(element.contents())(scope); // compile the template
+         scope.setSelectedRow = function(selection) {
+             console.info('selection is set: ', selection);
+             ngModelCtrl.$setViewValue(selection); // set the value of the ng-model with the selection
+         }; // setSelectedRow to be used in the below controller
+      } // linkerFn
 
-          scope.lookupInterventions = function(params) {
+      function nciInterventionsSearchCtrl($scope, $element, $attrs) {
+          var vm = this;
+          vm.lookupInterventions = lookupInterventions;
+          vm.resetSearch = resetSearch;
+          vm.searchParams = _getSearchParams();
+          vm.searchResults = _initResultsObj();
+          vm.gridOptions = _getGridOptions();
+
+          function lookupInterventions(params) {
               PATrialService.lookupNcitInterventions(params).then(function(res) {
                  console.info('res from the interventions lookup: ', res);
                  res.server_response = null;
-                 scope.searchResults = res;
-                 scope.gridOptions.api.setRowData(res.data);
-                 scope.gridOptions.api.refreshView();
+                 vm.searchResults = res;
+                 vm.gridOptions.api.setRowData(res.data);
+                 vm.gridOptions.api.refreshView();
               }).catch(function(err) {
                   console.error('err in the lookup: ', err);
-                  scope.searchResults = _initResultsObj();
+                  vm.searchResults = _initResultsObj();
               });
           };
 
-          scope.resetSearch = function() {
-              scope.searchParams = _getSearchParams();
-              scope.gridOptions.api.setRowData([]);
+         function resetSearch() {
+              vm.searchParams = _getSearchParams();
+              vm.gridOptions.api.setRowData([]);
           };
 
-          function getGridOptions() {
+          function _getGridOptions() {
 
               var options = {
                    enableServerSideSorting: true,
                    enableServerSideFilter: true,
                    rowModelType: 'pagination',
                    columnDefs: getColumnDefs(),
-                   rowSelection: scope.maxRowSelectable > 1 ? 'multiple' : 'single',
+                   rowSelection: vm.maxRowSelectable > 1 ? 'multiple' : 'single',
                 //   onSelectionChanged: onRowSelectionChanged, // for all rows
                    onRowSelected: rowSelectedCallback, // current selected row (single row)
                    enableColResize: true,
@@ -74,15 +82,15 @@
                };
                // options.datasource = tableDataSource;
                return options;
-          } // getGridOptions
+          } // _getGridOptions
 
           function rowSelectedCallback(event) {
-              console.info('is node selected? ', scope.gridOptions.api.isNodeSelected(event.node), event.node.id);
+            //   console.info('is node selected? ', vm.gridOptions.api.isNodeSelected(event.node), event.node.id);
               var curSelectedNode = event.node;
-              console.log('node props: ', curSelectedNode);
+            //   console.log('node props: ', curSelectedNode);
               var curSelectedRowObj = curSelectedNode.data;
-              ngModelCtrl.$setViewValue(curSelectedRowObj); // set the value of the ng-model with the selection
-              var selectedRows = scope.gridOptions.api.getSelectedRows();
+              $scope.setSelectedRow(curSelectedRowObj); // method from linkerFn
+              var selectedRows = vm.gridOptions.api.getSelectedRows();
           }
 
           function onModelUpdated() {
@@ -106,15 +114,15 @@
 
           var tableDataSource = {
               getRows: getRows,
-              pageSize: scope.searchParams.rows,
+              pageSize: vm.searchParams.rows,
               rowCount: 100,
               overflowSize: 100,
               maxConcurrentRequests: 3
           };
 
           function getRows(params) {
-              params.startRow = (scope.searchParams.start - 1) * scope.searchParams.rows;
-              params.endRow = scope.searchParams.start * scope.searchParams.rows;
+              params.startRow = (vm.searchParams.start - 1) * vm.searchParams.rows;
+              params.endRow = vm.searchParams.start * vm.searchParams.rows;
               params.successCallback = function() {
                   console.info('success in getRows');
               }
@@ -122,7 +130,7 @@
           }
 
           function _initResultsObj() {
-              var results = angular.copy(scope.searchParams);
+              var results = angular.copy(vm.searchParams);
               results.data = [];
               results.total = -1;
               return results;
@@ -137,9 +145,7 @@
                   intervention_name: '' // against either preferred_name or synonyms in ncit_interventions table
               };
           } // _getSearchParams
-
-
-      } // linkerFn
+      } // nciInterventionsSearchCtrl
 
 
   }
