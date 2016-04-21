33ceea203ae1fb60762135f6e224a7ee97054505
wus4 <shenpei.wu@gmail.com>
2016-04-21 17:41:12 -0400

    Saving milestone initial check-in

--- angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/paMilestoneCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/paMilestoneCtrl.js
@@ -32,6 +32,35 @@
             }
         }; //openCalendar
 
+        vm.saveMilestone = function() {
+            // Prevent multiple submissions
+            vm.disableBtn = true;
+
+            vm.curTrial.milestone_wrappers_attributes = [];
+            var milestoneWrapperObj = {};
+            milestoneWrapperObj.milestone_id = vm.milestone_id;
+            milestoneWrapperObj.milestone_date = vm.milestone_date;
+            milestoneWrapperObj.comment = vm.comment;
+            vm.curTrial.milestone_wrappers_attributes.push(milestoneWrapperObj);
+
+            // An outer param wrapper is needed for nested attributes to work
+            var outerTrial = {};
+            outerTrial.id = vm.curTrial.id;
+            outerTrial.trial = vm.curTrial;
+
+            TrialService.upsertTrial(outerTrial).then(function(response) {
+                if (response.server_response.status < 300) {
+                    $state.go('main.pa.trialOverview.milestone', {}, {reload: true});
+                    toastr.success('Milestone have been recorded', 'Operation Successful!');
+                } else {
+                    // Enable buttons in case of backend error
+                    vm.disableBtn = false;
+                }
+            }).catch(function(err) {
+                console.log("Error in saving milestone " + JSON.stringify(outerTrial));
+            });
+        };
+
         activate();
 
         /****************************** implementations **************************/
