c104e0701d847bfdb095bc725877dfa86d938dcf
wus4 <shenpei.wu@gmail.com>
2016-04-21 17:31:12 -0400

    Date picker

--- angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/paMilestoneCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/trial_overview/paMilestoneCtrl.js
@@ -7,17 +7,30 @@
     angular.module('ctrp.app.pa.dashboard')
         .controller('paMilestoneCtrl', paMilestoneCtrl);
 
-    paMilestoneCtrl.$inject = ['$scope', '$state', 'toastr', 'trialDetailObj', 'milestoneObj', 'TrialService'];
+    paMilestoneCtrl.$inject = ['$scope', '$state', 'toastr', 'trialDetailObj', 'milestoneObj', 'TrialService', 'DateService'];
 
-    function paMilestoneCtrl($scope, $state, toastr, trialDetailObj, milestoneObj, TrialService) {
+    function paMilestoneCtrl($scope, $state, toastr, trialDetailObj, milestoneObj, TrialService, DateService) {
         var vm = this;
         vm.curTrial = trialDetailObj;
         vm.addMode = false;
         vm.milestoneArr = milestoneObj;
+        vm.milestone_date_opened = false;
 
         vm.setAddMode = function(mode) {
             vm.addMode = mode;
         };
+
+        vm.dateFormat = DateService.getFormats()[1];
+        vm.dateOptions = DateService.getDateOptions();
+        vm.today = DateService.today();
+        vm.openCalendar = function ($event, type) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            if (type == 'milestone_date') {
+                vm.milestone_date_opened = !vm.milestone_date_opened;
+            }
+        }; //openCalendar
 
         activate();
 
