df93e80baae0af3c297fc52fbd341c0e84072be0
wus4 <shenpei.wu@gmail.com>
2016-05-05 16:14:47 -0400

    Removed test record loading on qa

--- db/seeds/qa.rb
+++ db/seeds/qa.rb
@@ -5,5 +5,5 @@ AppSetting.find_or_create_by(code: 'LOGIN_BULLETIN', name: 'Login Bulletin', des
 AppSetting.find_or_create_by(code: 'APP_RELEASE_MILESTONE', name: 'Application Release Milestone', description: 'Use this for identifying a milestone of a software release, e.g. 5.0 M1', value: 'S10.2 (ELT + seed)', big_value: '')
 
 #Seed test records for this environment
-load(Rails.root.join( 'db', 'seeds', "po_test_records.rb"))
-load(Rails.root.join( 'db', 'seeds', "trial_test_records.rb"))
+#load(Rails.root.join( 'db', 'seeds', "po_test_records.rb"))
+#load(Rails.root.join( 'db', 'seeds', "trial_test_records.rb"))
