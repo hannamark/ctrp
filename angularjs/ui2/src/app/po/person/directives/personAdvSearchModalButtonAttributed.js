985090eb11eeda510933e639493ef90d73fcd578
rcholic <ivytony@gmail.com>
2016-05-11 16:47:17 -0400

    new button directive for person search modal

---
+++ angularjs/ui2/src/app/po/person/directives/personAdvSearchModalButtonAttributed.js
@@ -0,0 +1,70 @@
+/**
+ * Created by wangg5 on 5/11/2016
+ *
+ * This directive is used as an attribute in a button element, example:
+ *         <button class="btn btn-primary" person-search-modal-button2 ng-model="personSearchView.selectedPersons" restriction-field>Search Person</button>
+ * the ng-model directive in the above button element receives the selected person(s);
+ *
+ * Note: If more than one (1) person is allowed for selection each time, then you have to add the attribute "max-row-selectable= number (int)" to
+ * the button, e.g.
+ * <button class="btn btn-primary" person-search-modal-button2 ng-model="personSearchView.selectedPersons" max-row-selectable=2 restriction-field>Search Person</button>
+ * You do not have to add the attribute "max-row-selectable" if one (1) person is allowed for selection
+ *
+ */
+
+(function() {
+    'use strict';
+    angular.module('ctrp.app.po')
+           .directive('personSearchModalButton2', personSearchModalButton2);
+
+           personSearchModalButton2.$inject = ['$uibModal', '$compile', '$timeout'];
+
+           function personSearchModalButton2($uibModal, $compile, $timeout) {
+              var directiveObj = {
+                  restrict: 'A',
+                  require: '^ngModel',
+                  scope: {
+                      maxRowSelectable: '=?',
+                      ngModel: '='
+                  },
+                  link: linkerFn
+              };
+
+              return directiveObj;
+
+              /* implementations below */
+              function linkerFn(scope, element, attrs, ngModelCtrl) {
+                  $compile(element.contents())(scope);
+                  console.info('in linkerFn of the personSearchModalButton2');
+                  var modalOpened = false;
+
+                  element.bind('click', function() {
+                      var modalInstance = $uibModal.open({
+                          animation: true,
+                          templateUrl: 'app/po/person/directives/advanced_person_search_form_modal.html',
+                          controller: 'advancedPersonSearchModalCtrl as advPersonSearchModalView',
+                          size: 'lg',
+                          resolve: {
+                              maxRowSelectable: function () {
+                                  return scope.maxRowSelectable || 1; // 1 as default
+                              }
+                          }
+                      }); // open
+                      var modalOpened = true;
+
+                      modalInstance.result.then(function(selection) {
+                          if (angular.isArray(selection) && selection.length > 0) {
+                              ngModelCtrl.$setViewValue(selection); // value type is Array
+                          }
+                          modalOpened = false;
+                      }, function() {
+                          // modal cancelled or closed
+                          console.error('person search modal cancelled or closed by user');
+                          modalOpened = false;
+                      }); // result.then
+                  }); // bind click
+
+              } // linkerFn
+
+           } // personSearchModalButton2
+})();
