5a37e651f35b0accdd087cd9ce4b43585175bdd4
wus4 <shenpei.wu@gmail.com>
2016-06-07 16:00:02 -0400

    Label fix

--- angularjs/ui2/src/app/pa/search/PATrialService.js
+++ angularjs/ui2/src/app/pa/search/PATrialService.js
@@ -98,7 +98,7 @@
                 {name: 'nih_nci_prog', enableSorting: true, minWidth: '130', width: '3%',
                     cellTemplate: '<div class="ui-grid-cell-contents tooltip-uigrid" title="{{COL_FIELD}}">' + '{{COL_FIELD CUSTOM_FILTERS}}</div>'
                 },
-                {name: 'internal_source', enableSorting: true, minWidth: '130', width: '3%',
+                {name: 'internal_source', displayName: 'Information Source', enableSorting: true, minWidth: '130', width: '3%',
                     cellTemplate: '<div class="ui-grid-cell-contents tooltip-uigrid" title="{{COL_FIELD}}">' + '{{COL_FIELD CUSTOM_FILTERS}}</div>'
                 }
             ]
