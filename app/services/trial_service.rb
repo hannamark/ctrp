534e0e79bc8f4532c810bee2f9c0a43d39f94715
wus4 <shenpei.wu@gmail.com>
2016-06-21 15:28:11 -0400

    Trial history json snapshot

--- app/services/trial_service.rb
+++ app/services/trial_service.rb
@@ -5,7 +5,12 @@ class TrialService
   end
 
   def get_json
-    return @trial.to_json(include: [:other_ids, :trial_status_wrappers])
+    return @trial.to_json(include: [:other_ids, :ind_ides, :grants, :trial_status_wrappers, :trial_funding_sources,
+                                    :trial_co_lead_orgs, :trial_co_pis, :oversight_authorities, :mail_logs, :onholds,
+                                    :associated_trials, :alternate_titles, :central_contacts, :outcome_measures,
+                                    :other_criteria, :markers, :interventions, :arms_groups, :sub_groups,
+                                    :participating_sites, :citations, :links, :diseases, :collaborators,
+                                    :trial_ownerships, :anatomic_site_wrappers, :trial_checkout_logs])
   end
 
   def save_history(trial_json)
