69c5158db57bfc0624de6d360fb84c200352c7fd
kigonyapa <peter.kigonya@nih.gov>
2016-06-27 15:14:06 -0400

    Pending replaces In Review, Only Active users show up in Trial Ownership

--- angularjs/ui2/src/app/user/userListCtrl.js
+++ angularjs/ui2/src/app/user/userListCtrl.js
@@ -172,8 +172,8 @@
             exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location"))
         };
 
-        AppSettingsService.getSettings({ setting: 'USER_STATUSES', json_path: URL_CONFIGS.USER_STATUSES}).then(function (response) {
-            vm.statusArr = response.data;
+         UserService.getUserStatuses().then(function (response) {
+             vm.statusArr = response.data;
             if (vm.curUser.role == 'ROLE_SITE-SU') {
                 vm.statusArrForROLESITESU = _.filter(vm.statusArr, function (item, index) {
                     return _.contains(['ACT', 'INR'], item.code);
@@ -184,11 +184,7 @@
                     return _.contains(['ACT', 'INR', 'REJ'], item.code);
                 });
             }
-        }).catch(function (err) {
-            vm.statusArr = [];
-            console.log("Error in retrieving USER_STATUSES " + err);
-        });
-
+         });
         //ui-grid plugin options
         vm.searchParams = new SearchParams;
         vm.gridOptions = gridOptions;
