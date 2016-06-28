2d40ce318b2710b0e85fbe5e9d18aa44fe6c94cc
rcholic <ivytony@gmail.com>
2016-06-28 13:49:00 -0400

    primary purpose description for other

--- angularjs/ui2/src/app/pa/dashboard/abstraction/trial_validation/submissionValidCtrl.js
+++ angularjs/ui2/src/app/pa/dashboard/abstraction/trial_validation/submissionValidCtrl.js
@@ -8,12 +8,13 @@
         .controller('submissionValidCtrl', submissionValidCtrl);
 
     submissionValidCtrl.$inject = ['$scope', '$timeout', 'trialPhaseArr', 'primaryPurposeArr',
-    'PATrialService'];
+    'PATrialService', '_'];
 
     function submissionValidCtrl($scope, $timeout, trialPhaseArr, primaryPurposeArr,
-        PATrialService) {
+        PATrialService, _) {
         var vm = this;
         vm.trialDetailObj = {};
+        vm.isOtherPrimaryPurpose = false;
         vm.trialPhaseArr = trialPhaseArr;
         vm.primaryPurposeArr = primaryPurposeArr;
 
@@ -27,10 +28,34 @@
         activate();
         function activate() {
             _getTrialDetailCopy();
+            _watchPrimaryPurpose();
         }
 
         function _getTrialDetailCopy() {
             vm.trialDetailObj = PATrialService.getCurrentTrialFromCache();
+        }
+
+        function _watchPrimaryPurpose() {
+            $scope.$watch(function() {return vm.trialDetailObj.primary_purpose_id;},
+                function(newVal, oldVal) {
+                if (newVal !== undefined && newVal !== null) {
+                    var curPrimaryPurposeObj = _.findWhere(vm.primaryPurposeArr, {id: newVal});
+                    vm.isOtherPrimaryPurpose = curPrimaryPurposeObj.name.toLowerCase().indexOf('other') > -1;
+                    // reset to original data or empty
+                    vm.trialDetailObj.primary_purpose_other = _resetValueForField('primary_purpose_other');
+                }
+            });
+        } // _watchPrimaryPurpose
+
+        /**
+         * Get the value for the fieldName in the cached trial object
+         * @param  {[type]} fieldName [description]
+         * @return {String}  a String value that could be a blank String
+         */
+        function _resetValueForField(fieldName) {
+            var cachedTrial = PATrialService.getCurrentTrialFromCache();
+            var val = cachedTrial[fieldName] || '';
+            return val;
         }
 
         function validateSubmission() {
