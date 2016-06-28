7f6ed3e08dcaecb05b219e8db3040fd17818b4bd
wus4 <shenpei.wu@gmail.com>
2016-06-28 16:18:36 -0400

    Rollback associations

--- app/services/trial_service.rb
+++ app/services/trial_service.rb
@@ -6,15 +6,355 @@ class TrialService
 
   def get_json
     return @trial.to_json(include: [:other_ids, :ind_ides, :grants, :trial_status_wrappers, :trial_funding_sources,
-                                    :trial_co_lead_orgs, :trial_co_pis, :oversight_authorities, :mail_logs, :onholds,
-                                    :associated_trials, :alternate_titles, :central_contacts, :outcome_measures,
-                                    :other_criteria, :markers, :interventions, :arms_groups, :sub_groups,
-                                    :participating_sites, :citations, :links, :diseases, :collaborators,
-                                    :trial_ownerships, :anatomic_site_wrappers, :trial_checkout_logs])
+                                    :oversight_authorities, :onholds, :associated_trials, :alternate_titles,
+                                    :central_contacts, :outcome_measures, :other_criteria, :markers, :interventions,
+                                    :arms_groups, :sub_groups, :participating_sites, :citations, :links, :diseases,
+                                    :collaborators, :trial_ownerships, :anatomic_site_wrappers])
   end
 
   def save_history(trial_json)
     TrialHistory.create(snapshot: trial_json, submission: @trial.current_submission)
+  end
+
+  def rollback(submission_id)
+    trial_history = TrialHistory.find_by_submission_id(submission_id)
+    # Parameters for native fields and deleting existing children
+    rollback_params = {}
+    # Parameters for reconstructing children
+    rollback_params2 = {}
+    rollback_params[:id] = trial_history.snapshot['id']
+    rollback_params[:lead_protocol_id] = trial_history.snapshot['lead_protocol_id']
+
+    # Delete existing children and reconstruct children from snapshot
+    other_ids_attributes = []
+    @trial.other_ids.each do |other_id|
+      other_id_hash = {id: other_id.id, _destroy: true}
+      other_ids_attributes.push(other_id_hash)
+    end
+    rollback_params[:other_ids_attributes] = other_ids_attributes
+    other_ids_attributes = []
+    trial_history.snapshot['other_ids'].each do |other_id|
+      other_id_hash = {protocol_id: other_id['protocol_id'], protocol_id_origin_id: other_id['protocol_id_origin_id']}
+      other_ids_attributes.push(other_id_hash)
+    end
+    rollback_params2[:other_ids_attributes] = other_ids_attributes
+
+    ind_ides_attributes = []
+    @trial.ind_ides.each do |ind_ide|
+      ind_ide_hash = {id: ind_ide.id, _destroy: true}
+      ind_ides_attributes.push(ind_ide_hash)
+    end
+    rollback_params[:ind_ides_attributes] = ind_ides_attributes
+    ind_ides_attributes = []
+    trial_history.snapshot['ind_ides'].each do |ind_ide|
+      ind_ide_hash = {ind_ide_type: ind_ide['ind_ide_type'], grantor: ind_ide['grantor'], nih_nci: ind_ide['nih_nci'],
+                      holder_type_id: ind_ide['holder_type_id'], ind_ide_number: ind_ide['ind_ide_number']}
+      ind_ides_attributes.push(ind_ide_hash)
+    end
+    rollback_params2[:ind_ides_attributes] = ind_ides_attributes
+
+    grants_attributes = []
+    @trial.grants.each do |grant|
+      grant_hash = {id: grant.id, _destroy: true}
+      grants_attributes.push(grant_hash)
+    end
+    rollback_params[:grants_attributes] = grants_attributes
+    grants_attributes = []
+    trial_history.snapshot['grants'].each do |grant|
+      grant_hash = {funding_mechanism: grant['funding_mechanism'], institute_code: grant['institute_code'],
+                    nci: grant['nci'], serial_number: grant['serial_number'],
+                    deletion_comment: grant['deletion_comment'], deleted_at: grant['deleted_at'],
+                    deleted_by_username: grant['deleted_by_username']}
+      grants_attributes.push(grant_hash)
+    end
+    rollback_params2[:grants_attributes] = grants_attributes
+
+    trial_status_wrappers_attributes = []
+    @trial.trial_status_wrappers.each do |trial_status_wrapper|
+      trial_status_wrapper_hash = {id: trial_status_wrapper.id, _destroy: true}
+      trial_status_wrappers_attributes.push(trial_status_wrapper_hash)
+    end
+    rollback_params[:trial_status_wrappers_attributes] = trial_status_wrappers_attributes
+    trial_status_wrappers_attributes = []
+    trial_history.snapshot['trial_status_wrappers'].each do |trial_status_wrapper|
+      trial_status_wrapper_hash = {status_date: trial_status_wrapper['status_date'],
+                                   why_stopped: trial_status_wrapper['why_stopped'],
+                                   trial_status_id: trial_status_wrapper['trial_status_id'],
+                                   comment: trial_status_wrapper['comment']}
+      trial_status_wrappers_attributes.push(trial_status_wrapper_hash)
+    end
+    rollback_params2[:trial_status_wrappers_attributes] = trial_status_wrappers_attributes
+
+    trial_funding_sources_attributes = []
+    @trial.trial_funding_sources.each do |trial_funding_source|
+      trial_funding_source_hash = {id: trial_funding_source.id, _destroy: true}
+      trial_funding_sources_attributes.push(trial_funding_source_hash)
+    end
+    rollback_params[:trial_funding_sources_attributes] = trial_funding_sources_attributes
+    trial_funding_sources_attributes = []
+    trial_history.snapshot['trial_funding_sources'].each do |trial_funding_source|
+      trial_funding_source_hash = {organization_id: trial_funding_source['organization_id']}
+      trial_funding_sources_attributes.push(trial_funding_source_hash)
+    end
+    rollback_params2[:trial_funding_sources_attributes] = trial_funding_sources_attributes
+
+    oversight_authorities_attributes = []
+    @trial.oversight_authorities.each do |oversight_authority|
+      oversight_authority_hash = {id: oversight_authority.id, _destroy: true}
+      oversight_authorities_attributes.push(oversight_authority_hash)
+    end
+    rollback_params[:oversight_authorities_attributes] = oversight_authorities_attributes
+    oversight_authorities_attributes = []
+    trial_history.snapshot['oversight_authorities'].each do |oversight_authority|
+      oversight_authority_hash = {country: oversight_authority['country'],
+                                  organization: oversight_authority['organization']}
+      oversight_authorities_attributes.push(oversight_authority_hash)
+    end
+    rollback_params2[:oversight_authorities_attributes] = oversight_authorities_attributes
+
+    associated_trials_attributes = []
+    @trial.associated_trials.each do |associated_trial|
+      associated_trial_hash = {id: associated_trial.id, _destroy: true}
+      associated_trials_attributes.push(associated_trial_hash)
+    end
+    rollback_params[:associated_trials_attributes] = associated_trials_attributes
+    associated_trials_attributes = []
+    trial_history.snapshot['associated_trials'].each do |associated_trial|
+      associated_trial_hash = {trial_identifier: associated_trial['trial_identifier'],
+                               identifier_type_id: associated_trial['identifier_type_id'],
+                               official_title: associated_trial['official_title'],
+                               research_category_name: associated_trial['research_category_name']}
+      associated_trials_attributes.push(associated_trial_hash)
+    end
+    rollback_params2[:associated_trials_attributes] = associated_trials_attributes
+
+    alternate_titles_attributes = []
+    @trial.alternate_titles.each do |alternate_title|
+      alternate_title_hash = {id: alternate_title.id, _destroy: true}
+      alternate_titles_attributes.push(alternate_title_hash)
+    end
+    rollback_params[:alternate_titles_attributes] = alternate_titles_attributes
+    alternate_titles_attributes = []
+    trial_history.snapshot['alternate_titles'].each do |alternate_title|
+      alternate_title_hash = {category: alternate_title['category'], title: alternate_title['title'],
+                              source: alternate_title['source']}
+      alternate_titles_attributes.push(alternate_title_hash)
+    end
+    rollback_params2[:alternate_titles_attributes] = alternate_titles_attributes
+
+    central_contacts_attributes = []
+    @trial.central_contacts.each do |central_contact|
+      central_contact_hash = {id: central_contact.id, _destroy: true}
+      central_contacts_attributes.push(central_contact_hash)
+    end
+    rollback_params[:central_contacts_attributes] = central_contacts_attributes
+    central_contacts_attributes = []
+    trial_history.snapshot['central_contacts'].each do |central_contact|
+      central_contact_hash = {phone: central_contact['phone'], email: central_contact['email'],
+                              central_contact_type_id: central_contact['central_contact_type_id'],
+                              person_id: central_contact['person_id'], extension: central_contact['extension'],
+                              fullname: central_contact['fullname']}
+      central_contacts_attributes.push(central_contact_hash)
+    end
+    rollback_params2[:central_contacts_attributes] = central_contacts_attributes
+
+    outcome_measures_attributes = []
+    @trial.outcome_measures.each do |outcome_measure|
+      outcome_measure_hash = {id: outcome_measure.id, _destroy: true}
+      outcome_measures_attributes.push(outcome_measure_hash)
+    end
+    rollback_params[:outcome_measures_attributes] = outcome_measures_attributes
+    outcome_measures_attributes = []
+    trial_history.snapshot['outcome_measures'].each do |outcome_measure|
+      outcome_measure_hash = {title: outcome_measure['title'], time_frame: outcome_measure['time_frame'],
+                              description: outcome_measure['description'],
+                              safety_issue: outcome_measure['safety_issue'],
+                              outcome_measure_type_id: outcome_measure['outcome_measure_type_id'],
+                              index: outcome_measure['index']}
+      outcome_measures_attributes.push(outcome_measure_hash)
+    end
+    rollback_params2[:outcome_measures_attributes] = outcome_measures_attributes
+
+    other_criteria_attributes = []
+    @trial.other_criteria.each do |other_criterium|
+      other_criterium_hash = {id: other_criterium.id, _destroy: true}
+      other_criteria_attributes.push(other_criterium_hash)
+    end
+    rollback_params[:other_criteria_attributes] = other_criteria_attributes
+    other_criteria_attributes = []
+    trial_history.snapshot['other_criteria'].each do |other_criterium|
+      other_criterium_hash = {criteria_type: other_criterium['criteria_type'],
+                              criteria_desc: other_criterium['criteria_desc'], index: other_criterium['index']}
+      other_criteria_attributes.push(other_criterium_hash)
+    end
+    rollback_params2[:other_criteria_attributes] = other_criteria_attributes
+
+    markers_attributes = []
+    @trial.markers.each do |marker|
+      marker_hash = {id: marker.id, _destroy: true}
+      markers_attributes.push(marker_hash)
+    end
+    rollback_params[:markers_attributes] = markers_attributes
+    markers_attributes = []
+    trial_history.snapshot['markers'].each do |marker|
+      marker_hash = {name: marker['name'], record_status: marker['record_status'],
+                     biomarker_use_id: marker['biomarker_use_id'],
+                     evaluation_type_other: marker['evaluation_type_other'],
+                     assay_type_other: marker['assay_type_other'], specimen_type_other: marker['specimen_type_other'],
+                     protocol_marker_name: marker['protocol_marker_name'], cadsr_marker_id: marker['cadsr_marker_id']}
+      markers_attributes.push(marker_hash)
+    end
+    rollback_params2[:markers_attributes] = markers_attributes
+
+    interventions_attributes = []
+    @trial.interventions.each do |intervention|
+      intervention_hash = {id: intervention.id, _destroy: true}
+      interventions_attributes.push(intervention_hash)
+    end
+    rollback_params[:interventions_attributes] = interventions_attributes
+    interventions_attributes = []
+    trial_history.snapshot['interventions'].each do |intervention|
+      intervention_hash = {name: intervention['name'], other_name: intervention['other_name'],
+                           description: intervention['description'],
+                           intervention_type_id: intervention['intervention_type_id'],
+                           index: intervention['index'], c_code: intervention['c_code']}
+      interventions_attributes.push(intervention_hash)
+    end
+    rollback_params2[:interventions_attributes] = interventions_attributes
+
+    arms_groups_attributes = []
+    @trial.arms_groups.each do |arms_group|
+      arms_group_hash = {id: arms_group.id, _destroy: true}
+      arms_groups_attributes.push(arms_group_hash)
+    end
+    rollback_params[:arms_groups_attributes] = arms_groups_attributes
+    arms_groups_attributes = []
+    trial_history.snapshot['arms_groups'].each do |arms_group|
+      arms_group_hash = {label: arms_group['label'], description: arms_group['description'],
+                         arms_groups_type: arms_group['arms_groups_type']}
+      arms_groups_attributes.push(arms_group_hash)
+    end
+    rollback_params2[:arms_groups_attributes] = arms_groups_attributes
+
+    sub_groups_attributes = []
+    @trial.sub_groups.each do |sub_group|
+      sub_group_hash = {id: sub_group.id, _destroy: true}
+      sub_groups_attributes.push(sub_group_hash)
+    end
+    rollback_params[:sub_groups_attributes] = sub_groups_attributes
+    sub_groups_attributes = []
+    trial_history.snapshot['sub_groups'].each do |sub_group|
+      sub_group_hash = {label: sub_group['label'], description: sub_group['description'], index: sub_group['index']}
+      sub_groups_attributes.push(sub_group_hash)
+    end
+    rollback_params2[:sub_groups_attributes] = sub_groups_attributes
+
+    participating_sites_attributes = []
+    @trial.participating_sites.each do |participating_site|
+      participating_site_hash = {id: participating_site.id, _destroy: true}
+      participating_sites_attributes.push(participating_site_hash)
+    end
+    rollback_params[:participating_sites_attributes] = participating_sites_attributes
+    participating_sites_attributes = []
+    trial_history.snapshot['participating_sites'].each do |participating_site|
+      participating_site_hash = {protocol_id: participating_site['protocol_id'],
+                                 program_code: participating_site['program_code'],
+                                 contact_name: participating_site['contact_name'],
+                                 contact_phone: participating_site['contact_phone'],
+                                 contact_email: participating_site['contact_email'],
+                                 organization_id: participating_site['organization_id'],
+                                 person_id: participating_site['person_id'], extension: participating_site['extension'],
+                                 contact_type: participating_site['contact_type'],
+                                 local_trial_identifier: participating_site['local_trial_identifier']}
+      participating_sites_attributes.push(participating_site_hash)
+    end
+    rollback_params2[:participating_sites_attributes] = participating_sites_attributes
+
+    citations_attributes = []
+    @trial.citations.each do |citation|
+      citation_hash = {id: citation.id, _destroy: true}
+      citations_attributes.push(citation_hash)
+    end
+    rollback_params[:citations_attributes] = citations_attributes
+    citations_attributes = []
+    trial_history.snapshot['citations'].each do |citation|
+      citation_hash = {pub_med_id: citation['pub_med_id'], description: citation['description'],
+                       results_reference: citation['results_reference']}
+      citations_attributes.push(citation_hash)
+    end
+    rollback_params2[:citations_attributes] = citations_attributes
+
+    links_attributes = []
+    @trial.links.each do |link|
+      link_hash = {id: link.id, _destroy: true}
+      links_attributes.push(link_hash)
+    end
+    rollback_params[:links_attributes] = links_attributes
+    links_attributes = []
+    trial_history.snapshot['links'].each do |link|
+      link_hash = {url: link['url'], description: link['description']}
+      links_attributes.push(link_hash)
+    end
+    rollback_params2[:links_attributes] = links_attributes
+
+    diseases_attributes = []
+    @trial.diseases.each do |disease|
+      disease_hash = {id: disease.id, _destroy: true}
+      diseases_attributes.push(disease_hash)
+    end
+    rollback_params[:diseases_attributes] = diseases_attributes
+    diseases_attributes = []
+    trial_history.snapshot['diseases'].each do |disease|
+      disease_hash = {preferred_name: disease['preferred_name'], code: disease['code'],
+                      thesaurus_id: disease['thesaurus_id'], display_name: disease['display_name'],
+                      parent_preferred: disease['parent_preferred'], rank: disease['rank']}
+      diseases_attributes.push(disease_hash)
+    end
+    rollback_params2[:diseases_attributes] = diseases_attributes
+
+    collaborators_attributes = []
+    @trial.collaborators.each do |collaborator|
+      collaborator_hash = {id: collaborator.id, _destroy: true}
+      collaborators_attributes.push(collaborator_hash)
+    end
+    rollback_params[:collaborators_attributes] = collaborators_attributes
+    collaborators_attributes = []
+    trial_history.snapshot['collaborators'].each do |collaborator|
+      collaborator_hash = {org_name: collaborator['org_name'], organization_id: collaborator['organization_id']}
+      collaborators_attributes.push(collaborator_hash)
+    end
+    rollback_params2[:collaborators_attributes] = collaborators_attributes
+
+    trial_ownerships_attributes = []
+    @trial.trial_ownerships.each do |trial_ownership|
+      trial_ownership_hash = {id: trial_ownership.id, _destroy: true}
+      trial_ownerships_attributes.push(trial_ownership_hash)
+    end
+    rollback_params[:trial_ownerships_attributes] = trial_ownerships_attributes
+    trial_ownerships_attributes = []
+    trial_history.snapshot['trial_ownerships'].each do |trial_ownership|
+      trial_ownership_hash = {user_id: trial_ownership['user_id']}
+      trial_ownerships_attributes.push(trial_ownership_hash)
+    end
+    rollback_params2[:trial_ownerships_attributes] = trial_ownerships_attributes
+
+    anatomic_site_wrappers_attributes = []
+    @trial.anatomic_site_wrappers.each do |anatomic_site_wrapper|
+      anatomic_site_wrapper_hash = {id: anatomic_site_wrapper.id, _destroy: true}
+      anatomic_site_wrappers_attributes.push(anatomic_site_wrapper_hash)
+    end
+    rollback_params[:anatomic_site_wrappers_attributes] = anatomic_site_wrappers_attributes
+    anatomic_site_wrappers_attributes = []
+    trial_history.snapshot['anatomic_site_wrappers'].each do |anatomic_site_wrapper|
+      anatomic_site_wrapper_hash = {anatomic_site_id: anatomic_site_wrapper['anatomic_site_id']}
+      anatomic_site_wrappers_attributes.push(anatomic_site_wrapper_hash)
+    end
+    rollback_params2[:anatomic_site_wrappers_attributes] = anatomic_site_wrappers_attributes
+
+    ActiveRecord::Base.transaction do
+      @trial.update(rollback_params)
+      @trial.update(rollback_params2)
+    end
   end
 
 end
