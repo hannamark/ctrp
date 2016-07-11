a7ebd178a72bfa6257f319dfe91be0ccd22e18da
wus4 <shenpei.wu@gmail.com>
2016-07-11 15:00:22 -0400

    Filter rejected trials in registry search

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -320,6 +320,7 @@ class TrialsController < ApplicationController
 
     elsif params[:protocol_id].present? || params[:official_title].present? || params[:phases].present? || params[:purposes].present? || params[:pilot].present? || params[:pi].present? || params[:org].present?  || params[:study_sources].present?
       @trials = Trial.all
+      @trials = @trials.filter_rejected
       @trials = @trials.with_protocol_id(params[:protocol_id]) if params[:protocol_id].present?
       @trials = @trials.matches_wc('official_title', params[:official_title]) if params[:official_title].present?
       @trials = @trials.with_phases(params[:phases]) if params[:phases].present?
