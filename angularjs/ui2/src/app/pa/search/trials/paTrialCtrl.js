cd3165ab14dc9d5aa1341768fcf26012892627f8
aasheer <aasheer1978@yahoo.com>
2016-09-22 17:58:42 -0400

    removed limitation of 999 from trial search

--- angularjs/ui2/src/app/pa/search/trials/paTrialCtrl.js
+++ angularjs/ui2/src/app/pa/search/trials/paTrialCtrl.js
@@ -51,8 +51,6 @@
             var origGridColumnDefs = angular.copy(vm.gridOptions.columnDefs);
 
             allSearchParams.start = null;
-            //allSearchParams.rows = null;
-            allSearchParams.rows = 999; // To get back all results, for now
 
             return PATrialService.searchTrialsPa(allSearchParams).then(
                 function (data) {
