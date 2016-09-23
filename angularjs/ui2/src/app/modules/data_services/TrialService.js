203bb258cd271e4cc8d32d88fcd372a1065c61cf
rcholic <ivytony@gmail.com>
2016-09-23 10:00:19 -0400

    disallow duplicate NCT id

--- angularjs/ui2/src/app/modules/data_services/TrialService.js
+++ angularjs/ui2/src/app/modules/data_services/TrialService.js
@@ -300,8 +300,9 @@
                 errorMsg = 'Both Protocol ID Origin and a Protocol ID are Required';
                 return errorMsg;
             }
-            var idObj = _.findWhere(addedOtherIds, {'protocol_id': protocolId});
-            var codeArr = ['OTH', 'ONCT', 'DNCI'];
+            // var idObj = _.findWhere(addedOtherIds, {'protocol_id': protocolId});
+            var idObj = _.findWhere(addedOtherIds, {'protocol_id_origin_id': protocolIdOriginId});
+            var codeArr = ['OTH', 'ONCT', 'DNCI']; // these code allow duplicates
             if (angular.isDefined(idObj) && !_.contains(codeArr, protocolIdOriginCode)) {
                 errorMsg = (idObj.protocol_id_origin_name || idObj.identifierName) + ' already exists';
                 return errorMsg;
