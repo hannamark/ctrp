6f89b47187c54964a5e05328fcb3543d3b54343e
dull7295 <murali@ou.edu>
2016-09-30 17:04:30 -0400

    rollback is refractored

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160929182704) do
+ActiveRecord::Schema.define(version: 20160930152929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
