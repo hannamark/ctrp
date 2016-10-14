d7841896e4922f5a366e0db00c8aeabc95cd07f2
rcholic <ivytony@gmail.com>
2016-10-14 16:35:48 -0400

    fixed search with service request

--- app/models/person.rb
+++ app/models/person.rb
@@ -35,6 +35,7 @@ class Person < ActiveRecord::Base
   has_many :organizations, through: :po_affiliations
   belongs_to :source_status
   belongs_to :source_context
+  belongs_to :service_request
   #belongs_to :source_cluster
   has_many :trial_co_pis
   has_many :copi_trials, through: :trial_co_pis, source: :trial
