4fdd27b25ff43c72e7446b0a2ee9f012024ee379
kigonyapa <peter.kigonya@nih.gov>
2016-10-18 08:54:18 -0400

    ing and setting search search to CTRP in org search modules

--- app/controllers/source_statuses_controller.rb
+++ app/controllers/source_statuses_controller.rb
@@ -16,9 +16,7 @@ class SourceStatusesController < ApplicationController
     org_source_status_access = (current_ctrp_user_role_details @current_user.role)[search_type]
 
     #search_context
-    if (['ROLE_ADMIN','ROLE_ACCOUNT-APPROVER','ROLE_SUPER','ROLE_ABSTRACTOR'].include? @current_user.role) && params[:view_context].empty?
-      @source_statuses = SourceStatus.all
-    elsif org_source_status_access
+    if org_source_status_access
       @source_statuses = SourceStatus.source_statuses_with_active_record_status
                              .where("source_context_id=?", SourceContext.find_by_code(search_context).id)
                              .order(code: :asc)
