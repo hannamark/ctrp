66880ebf06500c7b1fa7b290c33eaea13bf03506
kigonyapa <peter.kigonya@nih.gov>
2016-10-26 15:35:42 -0400

    updates

--- angularjs/ui2/src/app/modules/widgets/comment/CommentService.js
+++ angularjs/ui2/src/app/modules/widgets/comment/CommentService.js
@@ -52,7 +52,6 @@
           if (optionalField) {
             url = url.replace('.json', '/' + optionalField + '.json');
           }
-          $log.info('get comment list with url: ' + url);
           return PromiseTimeoutService.getData(url);
         }
         var deferred = $q.defer();
