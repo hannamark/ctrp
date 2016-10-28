0ae27c47dd8907fe3e916e9a738a8873626ea760
rcholic <ivytony@gmail.com>
2016-10-28 10:03:36 -0400

    changes to eslint config

--- angularjs/ui2/src/app/po/organization/directives/advancedOrgSearchForm2.js
+++ angularjs/ui2/src/app/po/organization/directives/advancedOrgSearchForm2.js
@@ -39,7 +39,7 @@
             // $compile(element.contents())(scope);
         } //linkFn
 
-        /*eslint max-statements: [1, 100, { "ignoreTopLevelFunctions": true }]*/
+        /*eslint max-statements: [1, 100]*/
         function ctrpAdvOrgSearchCtrl($scope) {
             var fromStateName = $state.fromState.name || '';
             var curStateName = $state.$current.name || '';
