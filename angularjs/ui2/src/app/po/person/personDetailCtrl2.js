6bbc3af7925a3939e33f356e61af930020f9aaa5
rcholic <ivytony@gmail.com>
2016-11-04 10:38:35 -0400

    fixed value

--- angularjs/ui2/src/app/po/person/personDetailCtrl2.js
+++ angularjs/ui2/src/app/po/person/personDetailCtrl2.js
@@ -322,7 +322,7 @@
                     if (status >= 200 && status <= 210) {
                         var curOrg = {"id" : poAff.organization_id, "name": organization.name};
                         var effectiveDate = !!poAff.effective_date ? moment(poAff.effective_date).toDate() : null;
-                        curOrg.effective_date = ; //DateService.convertISODateToLocaleDateStr(poAff.effective_date);
+                        curOrg.effective_date = effectiveDate; //DateService.convertISODateToLocaleDateStr(poAff.effective_date);
                         var expDate = !!poAff.expiration_date ? moment(poAff.expiration_date).toDate() : null; //DateService.convertISODateToLocaleDateStr(poAff.expiration_date);
                         curOrg.expiration_date = expDate;
                         curOrg.po_affiliation_status_id = poAff.po_affiliation_status_id;
