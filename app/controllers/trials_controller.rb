4e3456c5be31623557822b947e56fc4945c4e929
kigonyapa <peter.kigonya@nih.gov>
2016-11-08 10:01:42 -0500

    Organization typeahead - Add Address to the name in the typeahead suggestions

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -325,7 +325,7 @@ class TrialsController < ApplicationController
           @trials = Trial.matches('lead_org_id', current_user.organization_id).where(nih_nci_prog: nil).filter_rejected.active_submissions
         end
       end
-    elsif params[:org].present? || params[:protocol_id].present? || params[:official_title].present? || params[:phases].present? || params[:purposes].present? || params[:pilot].present? || params[:pi].present? || params[:org].present?  || params[:study_sources].present?
+    elsif params[:org].present? || params[:organization_id].present? || params[:protocol_id].present? || params[:official_title].present? || params[:phases].present? || params[:purposes].present? || params[:pilot].present? || params[:pi].present? || params[:org].present?  || params[:study_sources].present?
       @trials = Trial.filter_rejected
       @trials = @trials.with_protocol_id(params[:protocol_id]) if params[:protocol_id].present?
       @trials = matches_wc(@trials, 'official_title', params[:official_title], true) if params[:official_title].present?
@@ -333,6 +333,7 @@ class TrialsController < ApplicationController
       @trials = @trials.with_purposes(params[:purposes]) if params[:purposes].present?
       @trials = @trials.matches('pilot', params[:pilot]) if params[:pilot].present?
       @trials = @trials.with_org(params[:org], params[:org_types]) if params[:org].present?
+      @trials = @trials.with_org_id(params[:organization_id], params[:org_types]) if params[:organization_id].present?
       if params[:pi].present?
         splits = params[:pi].split(',').map(&:strip)
         @trials = @trials.with_pi_lname(splits[0])
